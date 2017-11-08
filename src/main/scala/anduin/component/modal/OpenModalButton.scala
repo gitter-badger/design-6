ab2c36d5b4701efee1c718175f1ae89e83392c1f
Phuoc Nguyen <phuoc@huuphuoc.me>
2017-11-08 13:49:11 +0700

    Display the turns inside a modal openning via stage name

---
+++ src/main/scala/anduin/component/modal/OpenModalButton.scala
@@ -0,0 +1,103 @@
+// Copyright (C) 2014-2017 Anduin Transactions Inc.
+
+package anduin.component.modal
+
+import japgolly.scalajs.react.vdom.TagOf
+import org.scalajs.dom.html.Anchor
+
+import anduin.component.util.JavaScriptUtils
+
+// scalastyle:off underscore.import
+import japgolly.scalajs.react._
+import japgolly.scalajs.react.vdom.html_<^._
+// scalastyle:on underscore.import
+
+// scalastyle:off parameter.number
+final case class OpenModalButton(
+  buttonLabel: String,
+  buttonClasses: String = "",
+  disabled: Boolean = false,
+  tip: String = "",
+  modalTitle: String = "",
+  modalClasses: String = "",
+  modalBody: Callback => VdomElement = _ => <.span(),
+  overlayCloseable: Boolean = true,
+  closeOnEscape: Boolean = true,
+  isOpen: Boolean = false,
+  onBeforeShowing: Callback = Callback.empty,
+  onAfterHiding: Callback = Callback.empty,
+  // Add key when we want to distinguish between different button
+  keyOpt: Option[String] = None
+) {
+  def apply(children: VdomNode*): ScalaComponent.Unmounted[_, _, _] = {
+    val ctorType = keyOpt.map(OpenModalButton.component.withKey(_)).getOrElse(OpenModalButton.component.ctor)
+    ctorType(this)(children: _*)
+  }
+}
+// scalastyle:on parameter.number
+
+object OpenModalButton {
+
+  private val componentName = this.getClass.getSimpleName
+
+  private case class State(isOpen: Boolean, over: Boolean)
+
+  private case class Backend(scope: BackendScope[OpenModalButton, State]) {
+
+    private def show = {
+      for {
+        props <- scope.props
+        _ <- props.onBeforeShowing
+        _ <- scope.modState(_.copy(isOpen = true))
+      } yield ()
+    }
+
+    private def hide = {
+      for {
+        props <- scope.props
+        _ <- props.onAfterHiding
+        _ <- scope.modState(_.copy(isOpen = false))
+      } yield ()
+    }
+
+    def render(props: OpenModalButton, state: State, children: PropsChildren): TagOf[Anchor] = {
+      <.a(
+        ^.href := JavaScriptUtils.VoidMethod,
+        ^.classSet(
+          s"dib ${props.buttonClasses}" -> true,
+          "disabled" -> props.disabled
+        ),
+        ^.onClick --> Callback.when(!props.disabled)(show),
+        TagMod.when(props.tip.nonEmpty) {
+          TagMod(
+            TagMod.when(state.over)(VdomAttr("data-tip") := props.tip),
+            ^.onMouseEnter --> Callback.when(!state.over)(scope.modState(_.copy(over = true))),
+            ^.onMouseLeave --> Callback.when(state.over)(scope.modState(_.copy(over = false)))
+          )
+        },
+        props.buttonLabel,
+        children,
+        TagMod.unless(props.disabled) {
+          Modal(
+            title = props.modalTitle,
+            isOpen = state.isOpen,
+            overlayClassName = props.modalClasses,
+            shouldCloseOnOverlayClick = props.overlayCloseable,
+            shouldCloseOnEscape = props.closeOnEscape,
+            onRequestClose = hide
+          )(onClose => props.modalBody(onClose))
+        }
+      )
+    }
+  }
+
+  private val component = ScalaComponent.builder[OpenModalButton](componentName)
+    .initialStateFromProps { props =>
+      State(
+        isOpen = props.isOpen,
+        over = false
+      )
+    }
+    .renderBackendWithChildren[Backend]
+    .build
+}
---
+++ src/main/scala/anduin/component/util/JavaScriptUtils.scala
@@ -0,0 +1,8 @@
+// Copyright (C) 2014-2017 Anduin Transactions Inc.
+
+package anduin.component.util
+
+object JavaScriptUtils {
+
+  final val VoidMethod = "javascript: void(0);"
+}
