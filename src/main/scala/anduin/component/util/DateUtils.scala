f8dab5d5b65f5ba28cde847638e0f48dac50f318
Ho Sy Viet Anh <vietanhho@anduintransact.com>
2017-12-13 18:23:07 +0700

    Move some utils to stargazerJS

---
+++ src/main/scala/anduin/component/util/DateUtils.scala
@@ -0,0 +1,44 @@
+// Copyright (C) 2014-2017 Anduin Transactions Inc.
+
+package anduin.component.util
+
+import scala.scalajs.js.{Date => JsDate}
+
+import org.threeten.bp.{Duration, Instant, LocalDate, ZonedDateTime}
+
+import anduin.scalajs.moment.{Date, Moment}
+
+object DateUtils {
+
+  def toJsDate(date: ZonedDateTime): JsDate = {
+    new JsDate(date.toInstant.toEpochMilli.toDouble)
+  }
+
+  def toJsDate(date: LocalDate): JsDate = {
+    new JsDate(Duration.ofDays(date.toEpochDay).toMillis.toDouble)
+  }
+
+  def rollToNextMondayIfWeekend(date: ZonedDateTime): ZonedDateTime = {
+    val jsDate = toJsDate(date)
+    // Note: `getDay()` returns (0-6) which is Sunday -> Saturday
+    jsDate.getDay() match {
+      case 0 => date.plusDays(1) // Sunday roll to Monday
+      case 6 => date.plusDays(2) // Saturday roll to Monday
+      case _ => date
+    }
+  }
+
+  implicit def toMomentDate(date: ZonedDateTime): Date = {
+    Moment(date.toInstant.toEpochMilli.toDouble)
+  }
+
+  implicit def toMomentDate(instant: Instant): Date = {
+    Moment(instant.toEpochMilli.toDouble)
+  }
+
+  val Day = "day"
+
+  val Month = "month"
+
+  val Year = "year"
+}
---
+++ src/main/scala/anduin/component/util/JsDateFormatterUtils.scala
@@ -0,0 +1,53 @@
+// Copyright (C) 2014-2017 Anduin Transactions Inc.
+
+package anduin.component.util
+
+import org.threeten.bp.{Instant, LocalDate, LocalDateTime, ZonedDateTime}
+
+import anduin.scalajs.moment.Moment
+
+// scalastyle:off underscore.import
+import anduin.component.util.DateUtils._
+// scalastyle:on underscore.import
+
+/**
+  * Date formatter on JS side. We should replace this with `CoreFormatterUtils` when scala-java-time supports time zone
+  * Formats can be looked up at https://momentjs.com
+  */
+object JsDateFormatterUtils {
+
+  final val DefaultPattern = "" // 2016-11-01T15:33:50+07:00
+  final val CreatedAtDatePattern = "hh:mm A" // 07:40 AM
+  final val LongDatePattern = "MMM DD YYYY" // Dec 12 2015
+  final val SuperLongDatePattern = "YYYY-MM-DD hh:mm:ss" // 2016-10-19 12:40:30
+  final val LongDatePattern1 = "MMM Do, YYYY" // May 15th, 2016
+
+  def format(date: ZonedDateTime, pattern: String): String = date.format(pattern)
+  def format(instant: Instant, pattern: String): String = instant.format(pattern)
+
+  private final val FourDigitZeroPaddingFormatter = "%04d" // return "1992" or "0082"
+  private final val TwoDigitZeroPaddingFormatter = "%02d" // return "11" or "02"
+  private final val LocalDatePattern = "YYYY-MM-DD"
+  private final val LocalDateTimePattern = "YYYY-MM-DD hh:mm:ss"
+
+  def format(date: LocalDate, pattern: String): String = {
+    val yearStr = FourDigitZeroPaddingFormatter.format(date.getYear)
+    val monthStr = TwoDigitZeroPaddingFormatter.format(date.getMonthValue)
+    val dayStr = TwoDigitZeroPaddingFormatter.format(date.getDayOfMonth)
+
+    val dateStr = s"$yearStr-$monthStr-$dayStr" // e.g. "2015-12-25"
+    Moment(dateStr, LocalDatePattern).format(pattern)
+  }
+
+  def format(date: LocalDateTime, pattern: String): String = {
+    val yearStr = FourDigitZeroPaddingFormatter.format(date.getYear)
+    val monthStr = TwoDigitZeroPaddingFormatter.format(date.getMonthValue)
+    val dayStr = TwoDigitZeroPaddingFormatter.format(date.getDayOfMonth)
+    val hourStr = TwoDigitZeroPaddingFormatter.format(date.getHour)
+    val minuteStr = TwoDigitZeroPaddingFormatter.format(date.getMinute)
+    val secondStr = TwoDigitZeroPaddingFormatter.format(date.getSecond)
+
+    val dateStr = s"$yearStr-$monthStr-$dayStr $hourStr:$minuteStr:$secondStr" // e.g. "2015-12-25 02:35:03"
+    Moment(dateStr, LocalDateTimePattern).format(pattern)
+  }
+}
