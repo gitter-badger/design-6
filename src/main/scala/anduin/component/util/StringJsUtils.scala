0e3d7bf1311fd4fb504d2a223b4d284185b3e3b1
Ho Sy Viet Anh <vietanhho@anduintransact.com>
2017-12-14 03:02:10 +0700

    Move StringJsUtils down

---
+++ src/main/scala/anduin/component/util/StringJsUtils.scala
@@ -0,0 +1,20 @@
+// Copyright (C) 2014-2017 Anduin Transactions Inc.
+
+package com.anduin.stargazer.client.utils
+
+import japgolly.scalajs.react.vdom.TagMod
+
+// scalastyle:off underscore.import
+import japgolly.scalajs.react.vdom.html_<^._
+// scalastyle:on underscore.import
+
+object StringJsUtils {
+
+  def newLineToBr(lines: List[String]): Array[TagMod] = {
+    lines.foldRight(Array.empty[TagMod])((l, acc) => if (acc.isEmpty) Array(l) else Array[TagMod](l, <.br) ++ acc)
+  }
+
+  def truncate(name: String, maxLength: Int = 40): String = {
+    if (name.length > maxLength) s"${name.substring(0, maxLength)} ..." else name
+  }
+}
