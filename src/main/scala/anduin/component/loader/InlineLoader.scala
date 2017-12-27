2618cda85b0ffdeaf51be54be027ff7e9a757467
iamquang95 <iamquang95@gmail.com>
2017-12-27 16:35:42 +0700

    Setting tab in rohan

---
+++ src/main/scala/anduin/component/loader/InlineLoader.scala
@@ -0,0 +1,25 @@
+// Copyright (C) 2014-2017 Anduin Transactions Inc.
+
+package anduin.component.loader
+
+import japgolly.scalajs.react.vdom.Exports.VdomTagOf
+
+// scalastyle:off underscore.import
+import japgolly.scalajs.react.vdom.html_<^._
+import org.scalajs.dom.html._
+// scalastyle:on underscore.import
+
+// TODO: replace all manual usages
+object InlineLoader {
+
+  // scalastyle:off multiple.string.literals
+  def apply(additionalClasses: String*): VdomTagOf[Span] = {
+    <.span(
+      ^.cls := s"loader-inline ${additionalClasses.mkString(" ")}",
+      <.span(^.cls := "item"),
+      <.span(^.cls := "item"),
+      <.span(^.cls := "item")
+    )
+  }
+  // scalastyle:off multiple.string.literals
+}
