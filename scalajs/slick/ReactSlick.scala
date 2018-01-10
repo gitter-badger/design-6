27ca03cc377f6cb8e1c664205d8ab6331e43b3a5
Phuoc Nguyen <phuoc@huuphuoc.me>
2018-01-10 15:52:40 +0700

    Add react-slick library for creating carousel

---
+++ scalajs/slick/ReactSlick.scala
@@ -0,0 +1,13 @@
+// Copyright (C) 2014-2018 Anduin Transactions Inc.
+
+package anduin.scalajs.slick
+
+import scala.scalajs.js
+import scala.scalajs.js.annotation.JSImport
+
+object ReactSlick {
+
+  @JSImport("react-slick", JSImport.Default)
+  @js.native
+  object RawComponent extends js.Object
+}
