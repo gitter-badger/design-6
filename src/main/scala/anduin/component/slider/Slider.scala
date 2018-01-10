f35397118817f97ebb7a70ada8c08b092e543e25
Phuoc Nguyen <phuoc@huuphuoc.me>
2018-01-10 15:52:40 +0700

    Add react-slick library for creating carousel

---
+++ src/main/scala/anduin/component/slider/Slider.scala
@@ -0,0 +1,35 @@
+// Copyright (C) 2014-2018 Anduin Transactions Inc.
+
+package anduin.component.slider
+
+import scala.scalajs.js
+
+import japgolly.scalajs.react.CtorType.ChildArg
+
+import anduin.scalajs.slick.ReactSlick
+
+// scalastyle:off underscore.import
+import japgolly.scalajs.react._
+// scalastyle:on underscore.import
+
+object Slider {
+
+  private val component: JsComponent[Props, Null, CtorType.PropsAndChildren] = {
+    JsComponent[Props, Children.Varargs, Null](ReactSlick.RawComponent)
+  }
+
+  def apply(
+    dots: Boolean = false
+  )(children: ChildArg*): JsComponent.Unmounted[_, _] = {
+    component(
+      new Props(
+        dots = dots
+      )
+    )(children: _*)
+  }
+
+  // See https://github.com/akiran/react-slick
+  final class Props(
+    val dots: Boolean = false
+  ) extends js.Object
+}
