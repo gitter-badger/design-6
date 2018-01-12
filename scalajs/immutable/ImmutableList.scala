42722f712a32b7d7adb04ca73360892179beb600
nghuuphuoc <phuoc@huuphuoc.me>
2018-01-12 21:09:37 +0700

    Fix an issue that causes error in console after undoing while composing email

--- scalajs/immutable/ImmutableList.scala
+++ scalajs/immutable/ImmutableList.scala
@@ -12,7 +12,8 @@ trait ImmutableList[T <: js.Object] extends js.Object {
   def filter(predicate: js.Function1[T, Boolean]) // linter:ignore UnusedParameter
     : ImmutableList[T] = js.native
 
-  def first(): T = js.native
+  // See https://facebook.github.io/immutable-js/docs/#/List/first
+  def first(): js.UndefOr[T] = js.native
 
   def toArray(): js.Array[T] = js.native
 }
