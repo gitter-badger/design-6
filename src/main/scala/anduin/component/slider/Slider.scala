2cb36ee5135fc5da18399732474c02b7b8fd035f
Phuoc Nguyen <phuoc@huuphuoc.me>
2018-01-22 14:51:41 +0700

    Change the Next button to Got it when being at the last slide

--- src/main/scala/anduin/component/slider/Slider.scala
+++ src/main/scala/anduin/component/slider/Slider.scala
@@ -4,8 +4,6 @@ package anduin.component.slider
 
 import scala.scalajs.js
 
-import japgolly.scalajs.react.component.Js.UnmountedWithRawType
-
 import anduin.scalajs.slick.ReactSlick
 
 // scalastyle:off underscore.import
@@ -19,18 +17,46 @@ object Slider {
     JsComponent[Props, Children.Varargs, Null](ReactSlick.RawComponent).addFacade[ReactSlick.RawComponent]
   }
 
-  def apply(props: Props)(children: VdomNode*): UnmountedWithRawType[_, _, _] = {
-    component(props)(children: _*)
+  // scalastyle:off parameter.number
+  def props(
+    arrows: Boolean = true,
+    dots: Boolean = false,
+    dotsClass: String = "slick-dots",
+    centerMode: Boolean = false,
+    centerPadding: String = "50px",
+    className: String = "",
+    customPaging: Int => raw.ReactElement = (i: Int) => <.button(i + 1).rawElement,
+    afterChange: Int => Callback = _ => Callback.empty,
+    beforeChange: (Int, Int) => Callback = (_: Int, _: Int) => Callback.empty
+  ): Props = {
+    new Props(
+      arrows = arrows,
+      dots = dots,
+      dotsClass = dotsClass,
+      centerMode = centerMode,
+      centerPadding = centerPadding,
+      className = className,
+      customPaging = customPaging,
+      afterChange = js.defined { index =>
+        afterChange(index).runNow()
+      },
+      beforeChange = js.defined { (currSlide: Int, nextSlide: Int) =>
+        beforeChange(currSlide, nextSlide).runNow()
+      }
+    )
   }
+  // scalastyle:on parameter.number
 
   // See https://github.com/akiran/react-slick
   final class Props(
-    val arrows: Boolean = true,
-    val dots: Boolean = false,
-    val dotsClass: String = "slick-dots",
-    val centerMode: Boolean = false,
-    val centerPadding: String = "50px",
-    val className: String = "",
-    val customPaging: js.Function1[Int, raw.ReactElement] = (i: Int) => <.button(i + 1).rawElement
+    val arrows: Boolean,
+    val dots: Boolean,
+    val dotsClass: String,
+    val centerMode: Boolean,
+    val centerPadding: String,
+    val className: String,
+    val customPaging: js.Function1[Int, raw.ReactElement],
+    val afterChange: js.UndefOr[js.Function1[Int, Unit]],
+    val beforeChange: js.UndefOr[js.Function2[Int, Int, Unit]]
   ) extends js.Object
 }
