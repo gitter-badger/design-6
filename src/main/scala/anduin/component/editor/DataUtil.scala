8cf608cb3e8b3d69c0efab68153067ac10b09aca
Phuoc Nguyen <phuoc@huuphuoc.me>
2018-01-24 15:35:54 +0700

    Fix an issue that couldn't parse the images attached in Live email

--- src/main/scala/anduin/component/editor/DataUtil.scala
+++ src/main/scala/anduin/component/editor/DataUtil.scala
@@ -7,6 +7,16 @@ import anduin.scalajs.slate.Slate.Data
 private[editor] object DataUtil {
 
   def value(data: Data, key: String, default: String = ""): String = {
-    data.get(key).toOption.map(_.asInstanceOf[String]).getOrElse(default)
+    data
+      .get(key)
+      .toOption
+      .map { d =>
+        val any: Any = d
+        any match {
+          case s: String => s
+          case _         => default
+        }
+      }
+      .getOrElse(default)
   }
 }
