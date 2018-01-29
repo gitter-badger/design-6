8a82c4da93f1b5a5f1700393b576a53fd33c182e
Phuoc Nguyen <phuoc@huuphuoc.me>
2018-01-29 15:01:34 +0700

    Remove the border when focus on the current page input

--- src/main/scala/anduin/component/input/AutoResize.scala
+++ src/main/scala/anduin/component/input/AutoResize.scala
@@ -19,18 +19,21 @@ object AutoResize {
   // See https://github.com/JedWatson/react-input-autosize
   final class Props(
     val value: String = "",
+    val inputClassName: String = "",
     val onChange: js.UndefOr[js.Function1[ReactEventFromInput, Unit]] = js.undefined,
     val onKeyDown: js.UndefOr[js.Function1[ReactKeyboardEventFromInput, Unit]] = js.undefined
   ) extends js.Object
 
   def apply(
     value: String,
+    inputClassName: String,
     onChange: ReactEventFromInput => Callback,
     onKeyDown: ReactKeyboardEventFromInput => Callback
   ): UnmountedWithRawType[_, _, _] = {
     component(
       new Props(
         value = value,
+        inputClassName = inputClassName,
         onChange = js.defined { v =>
           onChange(v).runNow()
         },
