f98779b46b6b5eb58389a7db5d06df46cc45e907
Phuoc Nguyen <phuoc@huuphuoc.me>
2018-03-21 09:53:18 +0700

    Introduce new Portal component

---
+++ src/main/scala/anduin/component/portal/Portal.scala
@@ -0,0 +1,57 @@
+// Copyright (C) 2014-2018 Anduin Transactions Inc.
+
+package anduin.component.portal
+
+import org.scalajs.dom
+import org.scalajs.dom.raw.Element
+
+// scalastyle:off underscore.import
+import japgolly.scalajs.react._
+import japgolly.scalajs.react.vdom.html_<^._
+// scalastyle:on underscore.import
+
+final case class Portal(
+  detachOnUnmount: Boolean = true
+) {
+  def apply(children: VdomNode*): VdomElement = {
+    Portal.component(this)(children: _*)
+  }
+}
+
+object Portal {
+
+  private val ComponentName = this.getClass.getSimpleName
+
+  private case class Backend(scope: BackendScope[Portal, _]) {
+
+    private var node: Element = _ // scalastyle:ignore
+
+    def componentWillUnmount: Callback = {
+      scope.props.flatMap { props =>
+        Callback.when(props.detachOnUnmount) {
+          Callback {
+            if (this.node != null) {
+              dom.document.body.removeChild(this.node)
+            }
+            this.node = null // scalastyle:ignore
+          }
+        }
+      }
+    }
+
+    def render(children: PropsChildren): VdomNode = {
+      if (node == null) { // scalastyle:ignore
+        node = dom.document.createElement("div")
+        dom.document.body.appendChild(node)
+      }
+      ReactPortal(children, node)
+    }
+  }
+
+  private val component = ScalaComponent
+    .builder[Portal](ComponentName)
+    .stateless
+    .renderBackendWithChildren[Backend]
+    .componentWillUnmount(_.backend.componentWillUnmount)
+    .build
+}
