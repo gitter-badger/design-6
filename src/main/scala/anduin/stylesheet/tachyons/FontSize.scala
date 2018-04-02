2ec55e5dabf031bea4ae7a92add40c85a7e26415
Thien Do <dvkndn@gmail.com>
2018-04-02 16:16:21 +0700

    Update font sizes

--- src/main/scala/anduin/stylesheet/tachyons/FontSize.scala
+++ src/main/scala/anduin/stylesheet/tachyons/FontSize.scala
@@ -2,9 +2,8 @@
 
 package anduin.stylesheet.tachyons
 
-// See https://github.com/tachyons-css/tachyons/blob/master/src/_type-scale.css
+// See /stargazer/gondor/webResources/src/main/assets/stylesheets/vendors/tachyons-4.9.1/_type-scale.css
 private[tachyons] final case class FontSize(classes: List[String] = List.empty) {
-
   def f1: Tachyons = new Tachyons(classes :+ "f1")
   def f2: Tachyons = new Tachyons(classes :+ "f2")
   def f3: Tachyons = new Tachyons(classes :+ "f3")
@@ -12,9 +11,4 @@ private[tachyons] final case class FontSize(classes: List[String] = List.empty) 
   def f5: Tachyons = new Tachyons(classes :+ "f5")
   def f6: Tachyons = new Tachyons(classes :+ "f6")
   def f7: Tachyons = new Tachyons(classes :+ "f7")
-
-  def hero6: Tachyons = new Tachyons(classes :+ "f-6")
-  def headline: Tachyons = new Tachyons(classes :+ "f-headline")
-  def hero5: Tachyons = new Tachyons(classes :+ "f-5")
-  def subHeadline: Tachyons = new Tachyons(classes :+ "f-subheadline")
 }
