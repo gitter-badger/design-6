4fbed694b6ce32ce4457c9153721fb845050393c
Thien Do <dvkndn@gmail.com>
2018-04-02 16:16:09 +0700

    Update font family

--- src/main/scala/anduin/stylesheet/tachyons/FontFamily.scala
+++ src/main/scala/anduin/stylesheet/tachyons/FontFamily.scala
@@ -4,20 +4,6 @@ package anduin.stylesheet.tachyons
 
 // See https://github.com/tachyons-css/tachyons/blob/master/src/_font-family.css
 private[tachyons] final case class FontFamily(classes: List[String] = List.empty) {
-
-  def sansSerif: Tachyons = new Tachyons(classes :+ "sans-serif")
-  def serif: Tachyons = new Tachyons(classes :+ "serif")
-  def systemSansSerif: Tachyons = new Tachyons(classes :+ "system-sans-serif")
-  def systemSerif: Tachyons = new Tachyons(classes :+ "system-serif")
-  def code: Tachyons = new Tachyons(classes :+ "code")
-  def courier: Tachyons = new Tachyons(classes :+ "courier")
-  def helvetica: Tachyons = new Tachyons(classes :+ "helvetica")
-  def avenir: Tachyons = new Tachyons(classes :+ "avenir")
-  def athelas: Tachyons = new Tachyons(classes :+ "athelas")
-  def georgia: Tachyons = new Tachyons(classes :+ "georgia")
-  def times: Tachyons = new Tachyons(classes :+ "times")
-  def bodoni: Tachyons = new Tachyons(classes :+ "bodoni")
-  def calisto: Tachyons = new Tachyons(classes :+ "calisto")
-  def garamond: Tachyons = new Tachyons(classes :+ "garamond")
-  def baskerville: Tachyons = new Tachyons(classes :+ "baskerville")
+  def sans: Tachyons = new Tachyons(classes :+ "ff-sans")
+  def mono: Tachyons = new Tachyons(classes :+ "ff-mono")
 }
