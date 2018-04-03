91cd498086129cbd5cdb94b182f9a0302423812b
Dat Nguyen <tiendatbk@gmail.com>
2018-04-03 19:07:32 +0700

    Fix broken master

--- src/main/scala/anduin/stylesheet/tachyons/LineHeight.scala
+++ src/main/scala/anduin/stylesheet/tachyons/LineHeight.scala
@@ -8,7 +8,4 @@ private[tachyons] final case class LineHeight(classes: List[String] = List.empty
   def l2: Tachyons = new Tachyons(classes :+ "lh-2")
   def l3: Tachyons = new Tachyons(classes :+ "lh-3")
   def copy: Tachyons = new Tachyons(classes :+ "lh-copy")
-
-  def l1: Tachyons = new Tachyons(classes :+ "lh-1")
-  def l2: Tachyons = new Tachyons(classes :+ "lh-2")
 }
