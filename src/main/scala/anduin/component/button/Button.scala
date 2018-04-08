bc8e109264beddb79f154d7fa75644ae2b3c33eb
Thien Do <dvkndn@gmail.com>
2018-04-08 17:45:10 +0700

    Button: Complete implementation
    - Use Style instead of css
    - Add full support like design: focus, disabled state, no color (minimal)
    - Simplify API
    - Support "link that looks like a button" usage with full a11y
    - No usage at the moment so nothing would be affected

--- src/main/scala/anduin/component/button/Button.scala
+++ src/main/scala/anduin/component/button/Button.scala
@@ -3,6 +3,7 @@
 package anduin.component.button
 
 import anduin.component.util.ComponentUtils
+import anduin.style.Style
 
 // scalastyle:off underscore.import
 import japgolly.scalajs.react._
@@ -10,12 +11,12 @@ import japgolly.scalajs.react.vdom.html_<^._
 // scalastyle:on underscore.import
 
 final case class Button(
-  intent: Button.Intent = Button.Intent.Default,
-  isMinimal: Boolean = false,
-  isDisable: Boolean = false,
-  size: Button.Size = Button.Size.Default,
-  tpe: Button.Tpe = Button.Tpe.Default,
-  onClick: Callback = Callback.empty
+  tpe: Button.Tpe = Button.TpeDefault,
+  color: Button.Color = Button.ColorGray,
+  size: Button.Size = Button.SizeMedium,
+  onClick: Callback = Callback.empty,
+  isDisabled: Boolean = false, // if tpe != link
+  href: String = "" // if tpe == link
 ) {
   def apply(children: VdomNode*): VdomElement = {
     Button.component(this)(children: _*)
@@ -24,35 +25,53 @@ final case class Button(
 
 object Button {
 
-  sealed trait Intent {
-    val className: String
-  }
-  case object Intent {
-    case object Default extends Intent { val className = "" }
-    case object Primary extends Intent { val className = "-primary" }
-    case object Success extends Intent { val className = "-success" }
-    case object Warning extends Intent { val className = "-warning" }
-    case object Danger extends Intent { val className = "-danger" }
-  }
-
-  sealed trait Size {
-    val className: String
-  }
-  case object Size {
-    case object Default extends Size { val className = "" }
-    case object Large extends Size { val className = "-large" }
-  }
-
-  sealed trait Tpe {
-    def tpe: String
-  }
-  case object Tpe {
-    case object Default extends Tpe { val tpe = "button" }
-    case object Submit extends Tpe { val tpe = "submit" }
-    case object Reset extends Tpe { val tpe = "reset" }
-  }
-
   private final val ComponentName = ComponentUtils.name(this)
+
+  sealed trait Color { val style: TagMod }
+  case object ColorNone extends Color {
+    val base: TagMod =
+      Style.color.gray8.hover.backgroundWhite.hover.colorPrimary4.active.backgroundGray2.active.colorPrimary4
+    val activeShadow: TagMod = Style.hover.shadowBorderGray4.active.shadowBorderGray4
+    val style: TagMod = TagMod(base, activeShadow)
+  }
+  case object ColorGray extends Color {
+    private val standby = Style.backgroundColor.gray1.shadow.borderGray4S
+    val style: TagMod = TagMod(ColorNone.base, standby)
+  }
+  private case object ColorCommon {
+    val style: TagMod = Style.color.white.shadow.size2
+  }
+  case object ColorPrimary extends Color {
+    private val standby = Style.backgroundColor.primary4.shadow.borderPrimary5S
+    private val active = Style.hover.backgroundPrimary3.active.backgroundPrimary5
+    val style: TagMod = TagMod(ColorCommon.style, standby, active)
+  }
+  case object ColorSuccess extends Color {
+    private val standby = Style.backgroundColor.success4.shadow.borderSuccess5S
+    private val active = Style.hover.backgroundSuccess3.active.backgroundSuccess5
+    val style: TagMod = TagMod(ColorCommon.style, standby, active)
+  }
+  case object ColorWarning extends Color {
+    private val standby = Style.backgroundColor.warning4.shadow.borderWarning5S
+    private val active = Style.hover.backgroundWarning3.active.backgroundWarning5
+    val style: TagMod = TagMod(ColorCommon.style, standby, active)
+  }
+  case object ColorDanger extends Color {
+    private val standby = Style.backgroundColor.danger4.shadow.borderDanger5S
+    private val active = Style.hover.backgroundDanger3.active.backgroundDanger5
+    val style: TagMod = TagMod(ColorCommon.style, standby, active)
+  }
+
+  sealed trait Tpe { val value: String }
+  case object TpeLink extends Tpe { val value = "link" }
+  case object TpeDefault extends Tpe { val value = "button" }
+  case object TpeSubmit extends Tpe { val value = "submit" }
+  case object TpeReset extends Tpe { val value = "reset" }
+
+  sealed trait Size { val style: TagMod }
+  case object SizeLarge extends Size { val style: TagMod = Style.padding.vertical3.padding.horizontal4 }
+  case object SizeMedium extends Size { val style: TagMod = Style.padding.vertical2.padding.horizontal4 }
+  case object SizeSmall extends Size { val style: TagMod = Style.padding.vertical1.padding.horizontal2 }
 
   private case class Backend(scope: BackendScope[Button, _]) {
 
@@ -66,18 +85,29 @@ object Button {
     }
 
     def render(props: Button, children: PropsChildren): VdomElement = {
-      <.button(
-        ^.tpe := props.tpe.toString,
-        ^.disabled := props.isDisable,
-        ^.classSet(
-          "at-btn" -> true,
-          "-minimal" -> props.isMinimal,
-          props.intent.className -> true,
-          props.size.className -> true
-        ),
+      val commonMods = TagMod(
+        // common styles for button
+        Style.lineHeight.size1.fontWeight.size500.borderRadius.r1,
+        // common disabled styles
+        Style.disabled.colorGray6.disabled.backgroundGray2,
+        Style.disabled.shadowBorderGray4,
+        // help with usage with icon
+        Style.flexbox.flex,
+        // specific styles
+        props.size.style,
+        props.color.style,
+        // behaviours
         ^.onClick ==> onClick,
         children
       )
+      props.tpe match {
+        // link that looks like a button
+        case TpeLink => <.a(^.href := props.href, commonMods)
+        // real button
+        case _ =>
+          val tpe = props.tpe.value
+          <.button(^.tpe := tpe, ^.disabled := props.isDisabled, commonMods)
+      }
     }
   }
 
