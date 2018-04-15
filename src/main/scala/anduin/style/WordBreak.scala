0e3e51b9078eae26325707ac5fc23020e0c2950b
nghuuphuoc <phuoc@huuphuoc.me>
2018-04-15 10:18:26 +0700

    Add break-word style to break long word

--- src/main/scala/anduin/style/WordBreak.scala
+++ src/main/scala/anduin/style/WordBreak.scala
@@ -6,4 +6,5 @@ private[style] final case class WordBreak(classes: List[String] = List.empty) {
   def normal: Style = new Style(classes :+ "word-normal")
   def wrap: Style = new Style(classes :+ "word-wrap")
   def noWrap: Style = new Style(classes :+ "word-nowrap")
+  def break: Style = new Style(classes :+ "word-break")
 }
