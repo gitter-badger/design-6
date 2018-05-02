9b4da2eb8daf20b3a30e64e47e5ff3f8b197762b
Thien Do <dvkndn@gmail.com>
2018-05-02 12:19:17 +0700

    [Style] Add 20px line-height

--- src/main/scala/anduin/style/LineHeight.scala
+++ src/main/scala/anduin/style/LineHeight.scala
@@ -4,6 +4,7 @@ package anduin.style
 
 private[style] final case class LineHeight(classes: List[String] = List.empty) {
   def px16: Style = new Style(classes :+ "lh-16")
+  def px20: Style = new Style(classes :+ "lh-20")
   def px24: Style = new Style(classes :+ "lh-24")
   def px40: Style = new Style(classes :+ "lh-40")
   def ratio1p5: Style = new Style(classes :+ "lh-r1p5")
