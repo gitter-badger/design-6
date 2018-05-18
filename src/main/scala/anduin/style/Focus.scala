2e04d963347d9b6ea0a5b74e6cb7eb987dbed13c
Thien Do <dvkndn@gmail.com>
2018-05-18 21:25:02 +0700

    [Style] Add dark option for Focus Outline

--- src/main/scala/anduin/style/Focus.scala
+++ src/main/scala/anduin/style/Focus.scala
@@ -4,6 +4,7 @@ package anduin.style
 
 private[style] final case class Focus(classes: List[String] = List.empty) {
   def outline: Style = new Style(classes :+ "fc-o")
+  def outlineDark: Style = new Style(classes :+ "fc-od")
   def border: Style = new Style(classes :+ "fc-b")
   def shadow: Style = new Style(classes :+ "fc-s")
 }
