d78e04247a2f57f14ffc41cdac417fa3626b3b5f
Thien Do <dvkndn@gmail.com>
2018-05-21 02:50:40 +0700

    [Style] Update Height's scale
    Use literal value for naming and support common values (24, 40, 48)

--- src/main/scala/anduin/style/Height.scala
+++ src/main/scala/anduin/style/Height.scala
@@ -3,11 +3,15 @@
 package anduin.style
 
 private[style] final case class Height(classes: List[String] = List.empty) {
-  def px16: Style = new Style(classes :+ "h1")
-  def px32: Style = new Style(classes :+ "h2")
-  def px64: Style = new Style(classes :+ "h3")
-  def px128: Style = new Style(classes :+ "h4")
-  def px256: Style = new Style(classes :+ "h5")
+  def px8: Style = new Style(classes :+ "h-8")
+  def px16: Style = new Style(classes :+ "h-16")
+  def px24: Style = new Style(classes :+ "h-24")
+  def px32: Style = new Style(classes :+ "h-32")
+  def px40: Style = new Style(classes :+ "h-40")
+  def px48: Style = new Style(classes :+ "h-48")
+  def px64: Style = new Style(classes :+ "h-64")
+  def px128: Style = new Style(classes :+ "h-128")
+  def px256: Style = new Style(classes :+ "h-256")
 
   def pc100: Style = new Style(classes :+ "h-100")
   def minPc100: Style = new Style(classes :+ "min-h-100")
