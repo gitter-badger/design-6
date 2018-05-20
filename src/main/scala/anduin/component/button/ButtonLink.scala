d55d2b498f2cd6ed21f5d172d85539bc2d47d65c
Thien Do <dvkndn@gmail.com>
2018-05-20 23:51:42 +0700

    [Component] Split ButtonLink from Button

---
+++ src/main/scala/anduin/component/button/ButtonLink.scala
@@ -0,0 +1,53 @@
+// Copyright (C) 2014-2018 Anduin Transactions Inc.
+
+package anduin.component.button
+
+// scalastyle:off underscore.import
+import japgolly.scalajs.react._
+import japgolly.scalajs.react.vdom.html_<^._
+// scalastyle:on underscore.import
+
+final case class ButtonLink(
+  // Common styling via ButtonStyle
+  color: ButtonStyle.Color = ButtonStyle.ColorWhite,
+  size: ButtonStyle.Size = ButtonStyle.SizeMedium,
+  style: ButtonStyle.Style = ButtonStyle.StyleFull,
+  isFullWidth: Boolean = false,
+  isSelected: Boolean = false,
+  // Specific behaviours for ButtonLink
+  onClick: Callback = Callback.empty, // link can still has onClick
+  href: String = "",
+  target: String = ""
+) {
+  def apply(children: VdomNode*): VdomElement = {
+    ButtonLink.component(this)(children: _*)
+  }
+}
+
+object ButtonLink {
+
+  type Props = ButtonLink
+
+  def render(props: Props, children: PropsChildren): VdomElement = <.a(
+    // styles
+    ButtonStyle.getStyles(
+      color = props.color,
+      size = props.size,
+      style = props.style,
+      isSelected = props.isSelected,
+      isFullWidth = props.isFullWidth
+    ),
+    // behaviours
+    ^.href := props.href,
+    ^.target := props.target,
+    TagMod.when(!props.onClick.isEmpty_?) { ^.onClick --> props.onClick },
+    // content
+    children
+  )
+
+  private val component = ScalaComponent
+    .builder[Props](this.getClass.getSimpleName)
+    .stateless
+    .render_PC(render)
+    .build
+}
