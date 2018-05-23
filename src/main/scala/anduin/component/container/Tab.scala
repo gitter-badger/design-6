4f42d8944a8fecafb754d57c5ad37b214d737e1a
Thien Do <dvkndn@gmail.com>
2018-05-23 14:44:43 +0700

    [Component] Tab's initial implementation

---
+++ src/main/scala/anduin/component/container/Tab.scala
@@ -0,0 +1,54 @@
+// Copyright (C) 2014-2018 Anduin Transactions Inc.
+
+package anduin.component.container
+
+// scalastyle:off underscore.import
+import japgolly.scalajs.react._
+import japgolly.scalajs.react.vdom.html_<^._
+// scalastyle:on underscore.import
+
+final case class Tab(
+  panels: List[Tab.Panel],
+  defaultPanel: Int = 0,
+  style: Tab.Style = Tab.StyleFull
+) {
+  def apply(): VdomElement = Tab.component(this)
+}
+
+object Tab {
+
+  // === Props
+
+  case class Panel(title: VdomNode, renderContent: () => VdomNode)
+
+  sealed trait Style
+  case object StyleMinimal extends Style
+  case object StyleFull extends Style
+
+  // === Backend
+
+  type Props = Tab
+
+  private case class State(active: Int)
+
+  private class Backend(scope: BackendScope[Props, State]) {
+
+    private def setActive(index: Int) = scope.setState { State(index) }
+
+    def render(props: Props, state: State): VdomElement = {
+      props.style match {
+        case StyleFull    => TabFull(props.panels, state.active, setActive)()
+        case StyleMinimal => TabFull(props.panels, state.active, setActive)()
+      }
+    }
+  }
+
+  // ===
+
+  private val component = ScalaComponent
+    .builder[Props](this.getClass.getSimpleName)
+    .initialStateFromProps(props => State(props.defaultPanel))
+    .renderBackend[Backend]
+    .build
+
+}
---
+++ src/main/scala/anduin/component/container/TabFull.scala
@@ -0,0 +1,74 @@
+// Copyright (C) 2014-2018 Anduin Transactions Inc.
+
+package anduin.component.container
+
+import anduin.style.Style
+
+// scalastyle:off underscore.import
+import japgolly.scalajs.react._
+import japgolly.scalajs.react.vdom.html_<^._
+// scalastyle:on underscore.import
+
+private[container] final case class TabFull(
+  panels: List[Tab.Panel],
+  active: Int,
+  setActive: Int => Callback
+) {
+  def apply(): VdomElement = { TabFull.component(this) }
+}
+
+private[container] object TabFull {
+
+  type Props = TabFull
+
+  private def renderButton(props: Props)(titleTup: (VdomNode, Int)): VdomElement = {
+    val (title, index) = titleTup
+    val isActive = props.active == index
+    <.button(
+      // === Styles
+      Style.focus.outline.transition.allWithOutline.padding.hor16.padding.ver12,
+      if (isActive) {
+        TagMod(Style.backgroundColor.white.color.gray8, ^.boxShadow := "white 0px 1px 0 0")
+      } else {
+        Style.backgroundColor.gray1.color.gray7.hover.backgroundWhite.active.backgroundGray2
+      },
+      TagMod.when(index != 0) { Style.border.left.borderColor.gray3 },
+      // === Behaviours
+      ^.tpe := "button",
+      ^.onClick --> props.setActive(index),
+      ^.disabled := isActive,
+      title
+    )
+  }
+
+  private def renderHeader(props: Props): VdomElement = {
+    <.header(
+      Style.border.bottom.borderColor.gray3,
+      <.div(
+        ^.marginBottom := "-1px",
+        Style.flexbox.inlineFlex.lineHeight.px16.fontWeight.medium,
+        Style.borderRadius.px2.borderRadius.top.border.all.borderColor.gray3,
+        props.panels.map(_.title).zipWithIndex.toTagMod { renderButton(props) }
+      )
+    )
+  }
+
+  private def renderContent(props: Props): VdomElement = {
+    <.div(
+      Style.backgroundColor.white.padding.all16.borderRadius.px2.borderRadius.bottom,
+      Style.border.left.border.bottom.border.right.borderColor.gray3,
+      props.panels(props.active).renderContent()
+    )
+  }
+
+  private def render(props: Props): VdomElement = {
+    <.div(renderHeader(props), renderContent(props))
+  }
+
+  private val component = ScalaComponent
+    .builder[Props](this.getClass.getSimpleName)
+    .stateless
+    .render_P(render)
+    .build
+
+}
