20f2f6d710e575b1d2ccd68f1feeae6513491c98
Thien Do <dvkndn@gmail.com>
2018-05-28 14:17:28 +0700

    [Component] Collapse: Update API
    Splitting renderTarget and renderContent is not necessary if the consumer must control the rendering anyway.
    
    Moreover, there are cases when it's not really seperated between target and content.
    
    Therefore, it's better to just let the consumer render both on their own

--- src/main/scala/anduin/component/container/Collapse.scala
+++ src/main/scala/anduin/component/container/Collapse.scala
@@ -8,33 +8,43 @@ import japgolly.scalajs.react.vdom.html_<^._
 // scalastyle:on underscore.import
 
 final case class Collapse(
-  renderTarget: (Callback, Boolean) => VdomNode,
-  renderContent: Boolean => VdomNode
+  render: (Callback, Boolean) => VdomNode,
+  isExpanded: Boolean = false
 ) {
   def apply(): VdomElement = Collapse.component(this)
 }
 
 object Collapse {
 
-  private case class State(toggled: Boolean = true)
+  type Props = Collapse
+
+  case class State(isExpanded: Boolean)
+
+  def getStateFromProps(props: Props): State = {
+    State(isExpanded = props.isExpanded)
+  }
 
   private class Backend(scope: BackendScope[Collapse, State]) {
 
-    private def toggle = scope.modState { state =>
-      state.copy(toggled = !state.toggled)
+    def modStateFromProps: Callback = {
+      scope.modState { (_, props) =>
+        getStateFromProps(props)
+      }
     }
 
-    def render(props: Collapse, state: State): VdomNode = {
-      ReactFragment(
-        props.renderTarget(toggle, state.toggled),
-        props.renderContent(state.toggled)
-      )
+    private def toggle = scope.modState { state =>
+      state.copy(isExpanded = !state.isExpanded)
+    }
+
+    def render(props: Props, state: State): VdomNode = {
+      props.render(toggle, state.isExpanded)
     }
   }
 
   private val component = ScalaComponent
-    .builder[Collapse](this.getClass.getSimpleName)
-    .initialState(State())
+    .builder[Props](this.getClass.getSimpleName)
+    .initialStateFromProps(this.getStateFromProps)
     .renderBackend[Backend]
+    .componentWillReceiveProps(_.backend.modStateFromProps)
     .build
 }
