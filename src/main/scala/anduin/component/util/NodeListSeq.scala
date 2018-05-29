651c900e0b266c6be7a840d392206c9488f72926
Phuoc Nguyen <phuoc@huuphuoc.me>
2018-05-29 17:22:43 +0700

    Add a form to add new comment

--- src/main/scala/anduin/component/util/NodeListSeq.scala
+++ src/main/scala/anduin/component/util/NodeListSeq.scala
@@ -2,10 +2,10 @@
 
 package anduin.component.util
 
-import org.scalajs.dom.{DOMList, Node}
+import org.scalajs.dom.DOMList
 
 // See https://www.scala-js.org/doc/sjs-for-js/es6-to-scala-part3.html
-case class NodeListSeq[T <: Node](nodes: DOMList[T]) extends IndexedSeq[T] {
+case class NodeListSeq[T](nodes: DOMList[T]) extends IndexedSeq[T] {
 
   override def foreach[U](f: T => U): Unit = {
     for (i <- 0 until nodes.length) {
