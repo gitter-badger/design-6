0169eec01dd8a54c46d3f6f1e8d4c949415b71b2
Hoang Phan <phan.minhhoang2606@gmail.com>
2018-06-21 15:51:29 +0700

    Added file input menu component

---
+++ src/main/scala/anduin/component/input/FileMenuItemInput.scala
@@ -0,0 +1,71 @@
+// Copyright (C) 2014-2018 Anduin Transactions Inc.
+
+package anduin.component.input
+
+import org.scalajs.dom.FileList
+
+import anduin.style.Style
+
+// scalastyle:off underscore.import
+import japgolly.scalajs.react._
+import japgolly.scalajs.react.vdom.html_<^._
+// scalastyle:on underscore.import
+
+final case class FileMenuItemInput(
+  onChange: FileList => Callback,
+  shouldClearAfterChanged: Boolean = false,
+  acceptTypes: String = "",
+  isMultiple: Boolean
+) {
+  def apply(children: VdomNode*): VdomElement = {
+    FileMenuItemInput.component(this)(children: _*)
+  }
+}
+
+object FileMenuItemInput {
+
+  private type Props = FileMenuItemInput
+
+  private class Backend(scope: BackendScope[Props, _]) {
+
+    private def onChange(e: ReactEventFromInput): Callback = {
+      for {
+        props <- scope.props
+        _ <- props.onChange(e.target.files)
+        _ <- Callback { e.target.value = "" }.when(props.shouldClearAfterChanged)
+      } yield ()
+    }
+
+    lazy val menuStyles = TagMod(
+      Style.color.gray8.lineHeight.px16,
+      Style.flexbox.flex.flexbox.itemsCenter.padding.ver8.padding.hor16,
+      Style.hover.colorWhite.active.colorWhite,
+      Style.hover.backgroundPrimary4.active.backgroundPrimary5,
+      Style.position.relative.width.pc100.textAlign.left,
+      Style.disabled.colorGray6.disabled.backgroundNone
+    )
+
+    def render(props: Props, children: PropsChildren): VdomElement = {
+      val input = <.input(
+        ^.tpe := "file",
+        Style.position.absolute.coordinate.fill.opacity.pc0.width.pc100.height.pc100.cursor.pointer,
+        ^.accept := props.acceptTypes,
+        ^.multiple := props.isMultiple,
+        ^.onChange ==> onChange
+      )
+      // Fake a menu item
+      <.div(
+        ^.tpe := "button",
+        menuStyles,
+        children,
+        input
+      )
+    }
+  }
+
+  private val component = ScalaComponent
+    .builder[Props](this.getClass.getSimpleName)
+    .stateless
+    .renderBackendWithChildren[Backend]
+    .build
+}
