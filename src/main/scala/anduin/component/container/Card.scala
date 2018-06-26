c7d8ef69269e2afc1e7955cd3065bd241d82050e
Thien Do <dvkndn@gmail.com>
2018-06-27 02:03:08 +0700

    [Component] Card has 20px padding and supports custom header

--- src/main/scala/anduin/component/container/Card.scala
+++ src/main/scala/anduin/component/container/Card.scala
@@ -2,14 +2,16 @@
 
 package anduin.component.container
 
+import anduin.style.Style
+
 // scalastyle:off underscore.import
 import japgolly.scalajs.react._
 import japgolly.scalajs.react.vdom.html_<^._
-
-import anduin.style.Style
 // scalastyle:on underscore.import
 
-final case class Card() {
+final case class Card(
+  header: VdomNode = EmptyVdom
+) {
   def apply(children: VdomNode*): VdomElement = {
     Card.component(this)(children: _*)
   }
@@ -17,12 +19,25 @@ final case class Card() {
 
 object Card {
 
-  type Props = Card
+  private type Props = Card
 
-  private def render(children: PropsChildren): VdomElement = {
+  private def renderHeader(props: Props): VdomNode = {
+    if (props.header == EmptyVdom) {
+      EmptyVdom
+    } else {
+      <.header(
+        Style.fontSize.px12.fontWeight.medium.margin.bottom20,
+        Style.color.gray7.textTransform.uppercase,
+        props.header
+      )
+    }
+  }
+
+  private def render(props: Props, children: PropsChildren): VdomElement = {
     <.div(
-      Style.backgroundColor.white.borderRadius.px2.padding.all16,
+      Style.backgroundColor.white.borderRadius.px2.padding.all20,
       Style.shadow.blur1Light,
+      renderHeader(props),
       children
     )
   }
@@ -30,6 +45,6 @@ object Card {
   private val component = ScalaComponent
     .builder[Props](this.getClass.getSimpleName)
     .stateless
-    .render_C(render)
+    .render_PC(render)
     .build
 }
