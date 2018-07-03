5b8e3583eebdb86446b7565eb6540a8e1a2da8b9
nghuuphuoc <phuoc@huuphuoc.me>
2018-07-03 16:49:14 +0700

    Make global CSS variables accessible from anduin.style

---
+++ src/main/scala/anduin/style/CssVar.scala
@@ -0,0 +1,64 @@
+// Copyright (C) 2014-2018 Anduin Transactions Inc.
+
+package anduin.style
+
+// The global CSS variables defined in `stylesheets/tachyons/variables` directory
+object CssVar {
+
+  object Color {
+    val gray1 = "var(--color-gray-1)"
+    val gray2 = "var(--color-gray-2)"
+    val gray3 = "var(--color-gray-3)"
+    val gray4 = "var(--color-gray-4)"
+    val gray5 = "var(--color-gray-5)"
+    val gray6 = "var(--color-gray-6)"
+    val gray7 = "var(--color-gray-7)"
+    val gray8 = "var(--color-gray-8)"
+    val gray9 = "var(--color-gray-9)"
+
+    val primary1 = "var(--color-primary-1)"
+    val primary2 = "var(--color-primary-2)"
+    val primary3 = "var(--color-primary-3)"
+    val primary4 = "var(--color-primary-4)"
+    val primary5 = "var(--color-primary-5)"
+
+    val success1 = "var(--color-success-1)"
+    val success2 = "var(--color-success-2)"
+    val success3 = "var(--color-success-3)"
+    val success4 = "var(--color-success-4)"
+    val success5 = "var(--color-success-5)"
+
+    val warning1 = "var(--color-warning-1)"
+    val warning2 = "var(--color-warning-2)"
+    val warning3 = "var(--color-warning-3)"
+    val warning4 = "var(--color-warning-4)"
+    val warning5 = "var(--color-warning-5)"
+
+    val danger1 = "var(--color-danger-1)"
+    val danger2 = "var(--color-danger-2)"
+    val danger3 = "var(--color-danger-3)"
+    val danger4 = "var(--color-danger-4)"
+    val danger5 = "var(--color-danger-5)"
+
+    val white = "var(--color-white)"
+  }
+
+  object Spacing {
+    val s0 = "var(--spacing-0)"
+    val s1 = "var(--spacing-1)"
+    val s2 = "var(--spacing-2)"
+    val s3 = "var(--spacing-3)"
+    val s4 = "var(--spacing-4)"
+    val s5 = "var(--spacing-5)"
+    val s6 = "var(--spacing-6)"
+    val s7 = "var(--spacing-7)"
+  }
+
+  object Shadow {
+    val border = "var(--shadow-border)"
+    val s1 = "var(--shadow-1)"
+    val s2 = "var(--shadow-2)"
+    val s3 = "var(--shadow-3)"
+    val s4 = "var(--shadow-4)"
+  }
+}
