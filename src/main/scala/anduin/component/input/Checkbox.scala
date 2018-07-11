3823ae8139722ae5b84380b5b9f9773a54e5216b
Thien Do <dvkndn@gmail.com>
2018-07-12 04:17:24 +0700

    [Cpn] Checkbox: Use max-content
    - To prevent clickable blank space
    - Also simplify its implementation

--- src/main/scala/anduin/component/input/Checkbox.scala
+++ src/main/scala/anduin/component/input/Checkbox.scala
@@ -24,8 +24,18 @@ final case class Checkbox(
 
 object Checkbox {
 
+  private type Props = Checkbox
+
   private val size = "20"
   private val sizePx = s"${size}px"
+
+  private def renderLabel(props: Checkbox, children: PropsChildren) = TagMod.when(children.nonEmpty) {
+    <.span(
+      Style.margin.left8,
+      TagMod.when(props.isDisabled) { Style.color.gray6 },
+      children
+    )
+  }
 
   private val tick = {
     svg_<^.<.svg(
@@ -34,6 +44,8 @@ object Checkbox {
       svg_<^.^.height := size,
       svg_<^.^.xmlns := "http://www.w3.org/2000/svg",
       svg_<^.^.fill := "currentColor",
+      // Neither the check mark in Icon nor the check unicode character is
+      // good enough, so we have our own SVG here:
       // scalastyle:off line.size.limit
       svg_<^.<.path(
         svg_<^.^.d := "M8.31 13.93l-3.16-2.98a.5.5 0 0 1 0-.71l1.07-1.08c.2-.2.5-.2.7-.01l1.57 1.39 4.59-4.4a.5.5 0 0 1 .7 0l1.07 1.08c.2.2.2.51 0 .7l-6.18 6c-.1.1-.26.1-.36 0z"
@@ -42,59 +54,54 @@ object Checkbox {
     )
   }
 
-  private class Backend(scope: BackendScope[Checkbox, _]) {
-    def render(props: Checkbox, children: PropsChildren): VdomElement = {
-      <.label(
-        Style.flexbox.flex.flexbox.itemsCenter.cursor.pointer.position.relative,
-        renderInput(props),
-        renderTick(props),
-        renderLabel(props, children)
-      )
-    }
+  private def renderTick(props: Checkbox) = TagMod.when(props.isChecked) {
+    <.span(
+      Style.position.absolute.coordinate.top0.coordinate.left0,
+      if (props.isDisabled) Style.color.gray6 else Style.color.white,
+      tick
+    )
+  }
 
-    private def renderLabel(props: Checkbox, children: PropsChildren) = TagMod.when(children.nonEmpty) {
-      <.span(
-        Style.margin.left8,
-        TagMod.when(props.isDisabled) { Style.color.gray6 },
-        children
-      )
-    }
+  private val inputStyles = TagMod(
+    ^.width := sizePx,
+    ^.height := sizePx,
+    Style.focus.outline.transition.allWithOutline,
+    Style.borderRadius.px2.border.all.cursor.pointer,
+    Style.disabled.backgroundGray2.disabled.borderGray4.disabled.shadowNone
+  )
 
-    private def renderTick(props: Checkbox) = TagMod.when(props.isChecked) {
-      <.span(
-        Style.position.absolute.coordinate.top0.coordinate.left0,
-        if (props.isDisabled) Style.color.gray6 else Style.color.white,
-        tick
-      )
-    }
+  private def renderInput(props: Checkbox) = {
+    <.input(
+      // behaviours
+      ^.tpe := "checkbox",
+      ^.disabled := props.isDisabled,
+      ^.checked := props.isChecked,
+      ^.onChange ==> { e: ReactEventFromInput =>
+        props.onChange(e.target.checked)
+      },
+      // styles
+      inputStyles,
+      if (props.isChecked) {
+        ButtonStyle.ColorPrimary.full
+      } else {
+        ButtonStyle.ColorWhite.full
+      }
+    )
+  }
 
-    private def renderInput(props: Checkbox) = {
-      <.input(
-        // behaviours
-        ^.tpe := "checkbox",
-        ^.disabled := props.isDisabled,
-        ^.checked := props.isChecked,
-        ^.onChange ==> onChange,
-        // styles
-        ^.width := sizePx,
-        ^.height := sizePx,
-        if (props.isChecked) ButtonStyle.ColorPrimary.full else ButtonStyle.ColorWhite.full,
-        Style.focus.outline.transition.allWithOutline.borderRadius.px2.border.all.cursor.pointer,
-        Style.disabled.backgroundGray2.disabled.borderGray4.disabled.shadowNone
-      )
-    }
-
-    private def onChange(e: ReactEventFromInput) = {
-      for {
-        props <- scope.props
-        _ <- props.onChange(e.target.checked)
-      } yield ()
-    }
+  private def render(props: Props, children: PropsChildren): VdomElement = {
+    <.label(
+      Style.flexbox.flex.flexbox.itemsCenter.width.maxContent,
+      Style.cursor.pointer.position.relative,
+      renderInput(props),
+      renderTick(props),
+      renderLabel(props, children)
+    )
   }
 
   private val component = ScalaComponent
-    .builder[Checkbox](this.getClass.getSimpleName)
+    .builder[Props](this.getClass.getSimpleName)
     .stateless
-    .renderBackendWithChildren[Backend]
+    .render_PC(render)
     .build
 }
