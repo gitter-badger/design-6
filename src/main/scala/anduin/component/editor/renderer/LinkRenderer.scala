6a3480be7f8da2c541130c56dc8f202df2a91867
nghuuphuoc <phuoc@huuphuoc.me>
2018-07-30 15:58:57 +0700

    Use Tooltip component to render menu of a link item

--- src/main/scala/anduin/component/editor/renderer/LinkRenderer.scala
+++ src/main/scala/anduin/component/editor/renderer/LinkRenderer.scala
@@ -4,9 +4,11 @@ package anduin.component.editor.renderer
 
 import scala.scalajs.js
 
-import japgolly.scalajs.react.{raw, PropsChildren}
+import japgolly.scalajs.react.{PropsChildren, raw}
 
 import anduin.component.editor.{DataUtil, StyleParser}
+import anduin.component.portal.{PositionBottom, Tooltip}
+import anduin.style.Style
 
 // scalastyle:off underscore.import
 import japgolly.scalajs.react.vdom.html_<^._
@@ -19,6 +21,26 @@ private[editor] object LinkRenderer {
   def apply(data: Data, children: js.Object): raw.React.Element = {
     val href = DataUtil.value(data, "href")
     val style = StyleParser.getStyleTagMod(data)
-    <.a(style, ^.href := href, PropsChildren.fromRawProps(js.Dynamic.literal(children = children))).rawElement
+
+    Tooltip(
+      position = PositionBottom,
+      targetTag = <.span,
+      trigger = (t, open, _, _) => {
+        TagMod(
+          ^.onClick --> open,
+          t
+        )
+      },
+      renderTarget = <.a(style, ^.href := href, PropsChildren.fromRawProps(js.Dynamic.literal(children = children))),
+      renderContent = () => {
+        <.a(
+          ^.cls := "link",
+          Style.color.white,
+          ^.href := href,
+          ^.target.blank,
+          "Open link"
+        )
+      }
+    )().rawElement
   }
 }
