085a0462cfdec939c332744c2fb674c0adffc563
Thien Do <dvkndn@gmail.com>
2018-08-14 13:31:40 +0700

    [Platform] Component: Add util to add QA ID Test

--- src/main/scala/anduin/component/util/ComponentUtils.scala
+++ src/main/scala/anduin/component/util/ComponentUtils.scala
@@ -2,12 +2,19 @@
 
 package anduin.component.util
 
-import japgolly.scalajs.react.vdom.html_<^.VdomAttr
+// scalastyle:off underscore.import
+import japgolly.scalajs.react.vdom.html_<^._
+// scalastyle:on underscore.import
 
 object ComponentUtils {
 
   // QA test id
+  // Deprecated, use the below one
   val DataTestID: VdomAttr[String] = VdomAttr[String]("data-test-id")
+
+  def testId(obj: Object, id: String): TagMod = {
+    VdomAttr("data-test-id") := s"${obj.getClass.getSimpleName}-$id"
+  }
 
   def name(obj: Object): String = {
     obj.getClass.getSimpleName.split('$').last
