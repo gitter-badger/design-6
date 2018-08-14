cb44da3e0272035f326715db24464bb2e24cd61c
Thien Do <dvkndn@gmail.com>
2018-08-14 13:31:05 +0700

    [Platform] Style: Add 32px line height

--- src/main/scala/anduin/style/LineHeight.scala
+++ src/main/scala/anduin/style/LineHeight.scala
@@ -6,6 +6,7 @@ private[style] final case class LineHeight(classes: List[String] = List.empty) {
   def px16: Style = new Style(classes :+ "lh-16")
   def px20: Style = new Style(classes :+ "lh-20")
   def px24: Style = new Style(classes :+ "lh-24")
+  def px32: Style = new Style(classes :+ "lh-32")
   def px40: Style = new Style(classes :+ "lh-40")
   def ratio1p5: Style = new Style(classes :+ "lh-r1p5")
   def copy: Style = ratio1p5
