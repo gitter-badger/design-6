aecbcbd76b1a62f729d1feda16ca06f97c2159ca
Thien Do <dvkndn@gmail.com>
2018-08-22 17:42:07 +0700

    [Portal] Drop legacy stuffs

--- src/main/scala/anduin/component/portal/Status.scala
+++
@@ -1,7 +0,0 @@
-// Copyright (C) 2014-2018 Anduin Transactions Inc.
-
-package anduin.component.portal
-
-sealed trait Status
-case object StatusOpen extends Status
-case object StatusClose extends Status
