1a2754fbe1fa5108c7964a19b54de93e6b775ee1
Thien Do <dvkndn@gmail.com>
2018-08-22 17:51:26 +0700

    [Portal] Safer and simpler Popper facades

--- scalajs/popper/Popper.scala
+++ scalajs/popper/Popper.scala
@@ -5,73 +5,15 @@ package anduin.scalajs.popper
 import scala.scalajs.js
 import scala.scalajs.js.annotation.JSImport
 
-import org.scalajs.dom.raw.{Element, HTMLElement}
+import org.scalajs.dom.raw.Element
 
 @JSImport("popper.js", JSImport.Default, "Popper")
 @js.native
-class Popper(val reference: Element, val popper: Element, val options: PopperOptions) extends js.Object {
+class Popper(
+  reference: Element,
+  popper: Element,
+  options: PopperOptions
+) extends js.Object {
   def destroy(): Unit = js.native
   def update(): Unit = js.native
 }
-
-object Popper extends js.Object {
-  type ModifierFn = js.Function2[Data, js.Object, Data]
-}
-
-final class PopperOptions(
-  // The placement can be
-  // - auto
-  // - top
-  // - right
-  // - bottom
-  // - left
-  // Each placement can have a variation
-  // - X-start
-  // - X-end
-  val placement: String = "bottom",
-  val positionFixed: Boolean = false,
-  val onCreate: js.Function1[Data, Unit] = (_: Data) => (),
-  val onUpdate: js.Function1[Data, Unit] = (_: Data) => (),
-  // TODO: I don't know why `js.UndefOr[js.Modifiers]` doesn't work
-  val modifiers: js.UndefOr[js.Object] = js.undefined
-) extends js.Object
-
-// See https://github.com/FezVrasta/popper.js/blob/HEAD/docs/_includes/popper-documentation.md#dataObject
-final class Data(
-  val instance: Popper,
-  val placement: String,
-  val originalPlacement: String,
-  val flipped: Boolean,
-  val hide: Boolean,
-  val arrowElement: HTMLElement,
-  val styles: js.Object,
-  val arrowStyles: js.Object,
-  val boundaries: js.Object,
-  val offsets: Offsets
-) extends js.Object
-
-final class Offsets(
-  val popper: Offset,
-  val reference: Offset,
-  val arrow: Offset
-) extends js.Object
-
-final class Offset(
-  val top: Double,
-  val left: Double,
-  val width: Double,
-  val height: Double
-) extends js.Object
-
-// Modifiers
-// See https://popper.js.org/popper-documentation.html#modifiers
-final class Modifiers(
-  val offset: js.UndefOr[OffsetModifier] = js.undefined
-) extends js.Object
-
-final class OffsetModifier(
-  val offset: String = "0",
-  val enabled: Boolean = true,
-  val order: Int = 200,
-  val fn: js.UndefOr[Popper.ModifierFn] = js.undefined
-) extends js.Object
---
+++ scalajs/popper/PopperOptions.scala
@@ -0,0 +1,83 @@
+// Copyright (C) 2014-2018 Anduin Transactions Inc.
+
+package anduin.scalajs.popper
+import scala.scalajs.js
+
+import org.scalajs.dom.raw.HTMLElement
+
+// Placement
+
+sealed abstract class Placement(val value: String)
+
+case object PlacementAutoStart extends Placement("auto-start")
+case object PlacementAuto extends Placement("auto")
+case object PlacementAutoEnd extends Placement("auto-end")
+case object PlacementTopStart extends Placement("top-start")
+case object PlacementTop extends Placement("top")
+case object PlacementTopEnd extends Placement("top-end")
+case object PlacementRightStart extends Placement("right-start")
+case object PlacementRight extends Placement("right")
+case object PlacementRightEnd extends Placement("right-end")
+case object PlacementBottomEnd extends Placement("bottom-end")
+case object PlacementBottom extends Placement("bottom")
+case object PlacementBottomStart extends Placement("bottom-start")
+case object PlacementLeftEnd extends Placement("left-end")
+case object PlacementLeft extends Placement("left")
+case object PlacementLeftStart extends Placement("left-start")
+
+// Data
+
+final class Data(
+  val arrowElement: js.UndefOr[HTMLElement]
+) extends js.Object
+
+// Modifiers
+
+final case class OffsetSimple(x: Double, y: Double)
+
+final class OffsetModifier(
+  val offset: String
+) extends js.Object
+
+object OffsetModifier {
+  def apply(
+    offset: OffsetSimple
+  ): OffsetModifier = new OffsetModifier(
+    offset = s"${offset.x.toString}px, ${offset.y.toString}px"
+  )
+}
+
+final class Modifiers(
+  val offset: js.UndefOr[OffsetModifier]
+) extends js.Object
+
+object Modifiers {
+  def apply(
+    offset: js.UndefOr[OffsetModifier] = js.undefined
+  ): Modifiers = new Modifiers(
+    offset
+  )
+}
+
+// This is JS interface (@ScalaJSDefine), so:
+// - It should have primitive type only and
+// - No default value
+final class PopperOptions(
+  val placement: String,
+  val onCreate: js.Function1[Data, Unit],
+  val modifiers: js.UndefOr[Modifiers]
+) extends js.Object
+
+// Here we can have custom types, default values and do conversions
+object PopperOptions {
+  def apply(
+    placement: Placement = PlacementBottom,
+    onCreate: Data => Unit = (_: Data) => (),
+    // avoid using Option here to avoid importing the costly JS Converters
+    modifiers: js.UndefOr[Modifiers] = js.undefined
+  ): PopperOptions = new PopperOptions(
+    placement.value,
+    onCreate,
+    modifiers
+  )
+}
