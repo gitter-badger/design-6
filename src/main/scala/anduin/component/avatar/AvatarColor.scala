9096986048738019d9d2b5d570543d7797a8c51a
nghuuphuoc <phuoc@huuphuoc.me>
2018-08-30 15:37:32 +0700

    Fix an issue that the avatar color is too light

--- src/main/scala/anduin/component/avatar/AvatarColor.scala
+++ src/main/scala/anduin/component/avatar/AvatarColor.scala
@@ -11,7 +11,7 @@ object AvatarColor {
   def apply(id: String): String = {
     // Taken from http://martin.ankerl.com/2009/12/09/how-to-create-random-colors-programmatically/
     val hash = MurmurHash3.stringHash(id) * GoldenRatioConjugate
-    val (h, s, l) = ((hash - Math.floor(hash)) * 360, 0.2 * 100, 0.8 * 100)
+    val (h, s, l) = ((hash - Math.floor(hash)) * 360, 0.45 * 100, 0.55 * 100)
     s"hsl($h,$s%,$l%)"
   }
 }
