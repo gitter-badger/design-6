6e24ce230ab9ae85e816d0783bc12c12dc3534c0
nghuuphuoc <phuoc@huuphuoc.me>
2018-09-04 17:12:26 +0700

    Bring back the class for opacity 10%

--- src/main/scala/anduin/style/Opacity.scala
+++ src/main/scala/anduin/style/Opacity.scala
@@ -4,6 +4,7 @@ package anduin.style
 
 private[style] final case class Opacity(classes: List[String] = List.empty) {
   def pc0: Style = new Style(classes :+ "o-0")
+  def pc10: Style = new Style(classes :+ "o-10")
   def pc20: Style = new Style(classes :+ "o-20")
   def pc50: Style = new Style(classes :+ "o-50")
   def pc100: Style = new Style(classes :+ "o-100")
