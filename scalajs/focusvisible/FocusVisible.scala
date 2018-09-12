3492c3dca1a523a733a0ade98c9c3bd9f60a318b
Thien Do <dvkndn@gmail.com>
2018-09-12 09:53:29 +0700

    [Platform] FocusVisible should belong to stargazerJSDep
    Previously it's in gondorJSDep, which is not really right since it should be available platform-wide.
    
    Also, `anduin.design` does not (and should not) link from `gondor` stuff. It only links from `platform`.

---
+++ scalajs/focusvisible/FocusVisible.scala
@@ -0,0 +1,10 @@
+// Copyright (C) 2014-2018 Anduin Transactions Inc.
+
+package anduin.scalajs.focusvisible
+
+import scala.scalajs.js
+import scala.scalajs.js.annotation.JSImport
+
+@JSImport("focus-visible", JSImport.Namespace, "FocusVisible")
+@js.native
+object FocusVisible extends js.Object
