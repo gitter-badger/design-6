4755f7fa0b915e2d1832fd7edfd8cdf2ef6572a4
nghuuphuoc <phuoc@huuphuoc.me>
2018-10-02 16:05:49 +0700

    Move OnlyOffice facade to anduin.scalajs

---
+++ scalajs/onlyoffice/Config.scala
@@ -0,0 +1,42 @@
+// Copyright (C) 2014-2018 Anduin Transactions Inc.
+
+package anduin.scalajs.onlyoffice
+
+import scala.scalajs.js
+
+// See https://api.onlyoffice.com/editors/config/
+class Config(
+  val editorConfig: Config.EditorConfig,
+  val document: Config.Document,
+  // Can be text, spreadsheet, presentation
+  val documentType: String = "text",
+  val height: String = "100%",
+  val width: String = "100%"
+) extends js.Object
+
+object Config {
+
+  // `document` options
+
+  class Document(
+    val fileType: String,
+    val key: String,
+    val title: String,
+    val url: String
+  ) extends js.Object
+
+  // `editorConfig` options
+
+  class EditorConfig(
+    val callbackUrl: String,
+    // Can be view or edit
+    val mode: String = "edit",
+    val customization: Customization
+  ) extends js.Object
+
+  class Customization(
+    val compactToolbar: Boolean = false,
+    val chat: Boolean = true,
+    val comments: Boolean = true
+  ) extends js.Object
+}
---
+++ scalajs/onlyoffice/Editor.scala
@@ -0,0 +1,19 @@
+// Copyright (C) 2014-2018 Anduin Transactions Inc.
+
+package anduin.scalajs.onlyoffice
+
+import scala.scalajs.js
+import scala.scalajs.js.annotation.JSGlobal
+
+@JSGlobal("DocsAPI.DocEditor")
+@js.native
+class Editor(
+  val id: String,
+  val config: Config
+) extends js.Object
+
+object Editor {
+  def apply(idParam: String, configParam: Config): Editor = {
+    new Editor(idParam, configParam)
+  }
+}
