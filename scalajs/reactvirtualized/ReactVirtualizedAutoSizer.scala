681f2271905d590e54ef89d8424bd634666307e4
Thien Do <dvkndn@gmail.com>
2018-10-11 15:26:10 +0700

    [Platform] Virtualized: Add facade

---
+++ scalajs/reactvirtualized/ReactVirtualizedAutoSizer.scala
@@ -0,0 +1,30 @@
+// Copyright (C) 2014-2018 Anduin Transactions Inc.
+
+package anduin.scalajs.reactvirtualized
+
+import scala.scalajs.js
+import scala.scalajs.js.annotation.JSImport
+
+// scalastyle:off underscore.import
+import japgolly.scalajs.react._
+// scalastyle:off underscore.import
+
+object ReactVirtualizedAutoSizer {
+
+  trait RenderProps extends js.Object {
+    val width: Int
+    val height: Int
+  }
+
+  class Props(
+    val children: js.Function1[RenderProps, raw.React.Node],
+    val disableWidth: js.UndefOr[Boolean] = js.undefined,
+    val disableHeight: js.UndefOr[Boolean] = js.undefined
+  ) extends js.Object
+
+  @JSImport("react-virtualized/dist/commonjs/AutoSizer", JSImport.Default)
+  @js.native
+  object RawComponent extends js.Object
+
+  val component = JsComponent[Props, Children.None, Null](RawComponent)
+}
