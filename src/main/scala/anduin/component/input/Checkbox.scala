3cf87c631dcf2a252fdef40b23d0f0a35130751a
Thien Do <dvkndn@gmail.com>
2018-11-08 09:47:49 +0700

    refactor(platform): Checkbox should use Icon
    Instead of inline SVG

--- src/main/scala/anduin/component/input/Checkbox.scala
+++ src/main/scala/anduin/component/input/Checkbox.scala
@@ -3,8 +3,8 @@
 package anduin.component.input
 
 import anduin.component.button.ButtonStyle
+import anduin.component.icon.Icon
 import anduin.style.Style
-import japgolly.scalajs.react.vdom.svg_<^
 
 // scalastyle:off underscore.import
 import japgolly.scalajs.react._
@@ -36,34 +36,17 @@ object Checkbox {
     )
   }
 
-  private val tick = {
-    svg_<^.<.svg(
-      Style.display.block,
-      svg_<^.^.width := size,
-      svg_<^.^.height := size,
-      svg_<^.^.xmlns := "http://www.w3.org/2000/svg",
-      svg_<^.^.fill := "currentColor",
-      // Neither the check mark in Icon nor the check unicode character is
-      // good enough, so we have our own SVG here:
-      // scalastyle:off line.size.limit
-      svg_<^.<.path(
-        svg_<^.^.d := "M8.31 13.93l-3.16-2.98a.5.5 0 0 1 0-.71l1.07-1.08c.2-.2.5-.2.7-.01l1.57 1.39 4.59-4.4a.5.5 0 0 1 .7 0l1.07 1.08c.2.2.2.51 0 .7l-6.18 6c-.1.1-.26.1-.36 0z"
-      )
-      // scalastyle:on line.size.limit
-    )
-  }
-
   private def renderTick(props: Checkbox) = TagMod.when(props.isChecked) {
     <.span(
-      Style.position.absolute.coordinate.top0.coordinate.left0,
+      Style.position.absolute.flexbox.flex.flexbox.justifyCenter.flexbox.itemsCenter,
+      TagMod(^.width := sizePx, ^.height := sizePx),
       if (props.isDisabled) Style.color.gray6 else Style.color.white,
-      tick
+      Icon(name = Icon.NameCheckBold)()
     )
   }
 
   private val inputStyles = TagMod(
-    ^.width := sizePx,
-    ^.height := sizePx,
+    TagMod(^.width := sizePx, ^.height := sizePx),
     Style.focus.outline.transition.allWithOutline,
     Style.borderRadius.px2.border.all.cursor.pointer,
     Style.disabled.backgroundGray2.disabled.borderGray4.disabled.shadowNone
@@ -75,16 +58,11 @@ object Checkbox {
       ^.tpe := "checkbox",
       ^.disabled := props.isDisabled,
       ^.checked := props.isChecked,
-      ^.onChange ==> { e: ReactEventFromInput =>
-        props.onChange(e.target.checked)
+      ^.onChange ==> { e: ReactEventFromInput => props.onChange(e.target.checked)
       },
       // styles
       inputStyles,
-      if (props.isChecked) {
-        ButtonStyle.ColorBlue.full
-      } else {
-        ButtonStyle.ColorWhite.full
-      }
+      if (props.isChecked) ButtonStyle.ColorBlue.full else ButtonStyle.ColorWhite.full
     )
   }
 
