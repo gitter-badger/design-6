39cdfe6b2febead00a4830a9d449e8e46503da3b
Hiep Bui <hiepbui@anduintransact.com>
2018-11-23 00:42:43 +0700

    Move view render methods to board consent project

---
+++ src/main/scala/anduin/component/portal/modal/ConfirmationModal.scala
@@ -0,0 +1,43 @@
+// Copyright (C) 2014-2018 Anduin Transactions Inc.
+
+package anduin.component.portal.modal
+
+import anduin.component.button.{Button, ButtonStyle}
+
+// scalastyle:off underscore.import
+import japgolly.scalajs.react._
+import japgolly.scalajs.react.vdom.html_<^._
+// scalastyle:on underscore.import
+
+final case class ConfirmationModal(
+  actionText: VdomNode,
+  onConfirm: Callback,
+  confirmBtnLabel: String,
+  confirmBtnColor: ButtonStyle.Color = ButtonStyle.ColorWhite,
+  onCloseModal: Callback,
+  isDisableConfirm: Boolean = false
+) {
+  def apply(): VdomElement = ConfirmationModal.component(this)
+}
+
+object ConfirmationModal {
+
+  private val ComponentName = this.getClass.getSimpleName
+
+  private val component = ScalaComponent
+    .builder[ConfirmationModal](ComponentName)
+    .stateless
+    .render_P { props =>
+      React.Fragment(
+        ModalBody()(props.actionText),
+        ModalFooterWCancel(cancel = props.onCloseModal)(
+          Button(
+            onClick = props.onConfirm,
+            color = props.confirmBtnColor,
+            isDisabled = props.isDisableConfirm
+          )(props.confirmBtnLabel)
+        )
+      )
+    }
+    .build
+}
