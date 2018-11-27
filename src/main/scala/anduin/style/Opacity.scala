120f6904b04e3ff2a7be48fed65756a62a2cd021
Cuong Manh Pham <phammanhcuong@gmail.com>
2018-11-27 15:11:22 +0700

    feat(Checklist): Replace Icon by Illus for elemental action buttons

--- src/main/scala/anduin/style/Opacity.scala
+++ src/main/scala/anduin/style/Opacity.scala
@@ -6,6 +6,7 @@ private[style] final case class Opacity(classes: List[String] = List.empty) {
   def pc0: Style = new Style(classes :+ "o-0")
   def pc10: Style = new Style(classes :+ "o-10")
   def pc20: Style = new Style(classes :+ "o-20")
+  def pc30: Style = new Style(classes :+ "o-30")
   def pc50: Style = new Style(classes :+ "o-50")
   def pc80: Style = new Style(classes :+ "o-80")
   def pc90: Style = new Style(classes :+ "o-90")
