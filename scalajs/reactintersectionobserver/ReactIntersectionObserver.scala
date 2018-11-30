ac340ffd17d67053bd4eea043fc14512a5465a00
nghuuphuoc <phuoc@huuphuoc.me>
2018-11-30 11:19:04 +0700

    Refactor and add more options for ReactIntersectionObserver

--- scalajs/reactintersectionobserver/ReactIntersectionObserver.scala
+++ scalajs/reactintersectionobserver/ReactIntersectionObserver.scala
@@ -5,6 +5,13 @@ package anduin.scalajs.reactintersectionobserver
 import scala.scalajs.js
 import scala.scalajs.js.annotation.JSImport
 
+import anduin.scalajs.intersectionobserver.IntersectionObserver
+
+// scalastyle:off underscore.import
+import japgolly.scalajs.react._
+import japgolly.scalajs.react.vdom.html_<^._
+// scalastyle:on underscore.import
+
 object ReactIntersectionObserver {
 
   @JSImport("@researchgate/react-intersection-observer", JSImport.Default, "Observer")
@@ -17,8 +24,30 @@ object ReactIntersectionObserver {
   }
 
   // See https://github.com/researchgate/react-intersection-observer#options
-  final class Props(
-    val disabled: Boolean = false,
-    val onChange: js.UndefOr[js.Function1[IntersectionObserverEntry, Unit]] = js.undefined
-  ) extends js.Object
+  trait Props extends js.Object {
+    val disabled: Boolean
+    val rootMargin: String
+    val threshold: Double
+    val onChange: js.UndefOr[js.Function1[IntersectionObserverEntry, Unit]]
+  }
+
+  val intersectionObserverPolyfill = IntersectionObserver
+  private val component = JsComponent[Props, Children.Varargs, Null](ReactIntersectionObserver.RawComponent)
+
+  def apply(
+    disabledParam: Boolean = false,
+    rootMarginParam: String = "0px 0px 0px 0px",
+    thresholdParam: Double = 0,
+    onChangeParam: IntersectionObserverEntry => Callback = _ => Callback.empty
+  )(children: VdomElement*): VdomElement = {
+    val props = new Props {
+      override val disabled = disabledParam
+      override val rootMargin = rootMarginParam
+      override val threshold = thresholdParam
+      override val onChange = js.defined { e =>
+        onChangeParam(e).runNow()
+      }
+    }
+    component(props)(children: _*)
+  }
 }
