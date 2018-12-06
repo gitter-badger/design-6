7e04069a3b754e66e0121c79f8d0972469f8257c
Hiep Bui <hiepbui@anduintransact.com>
2018-12-06 23:53:06 +0700

    Separate email modal in shared module

---
+++ src/main/scala/anduin/component/progressindicators/GeneralLoader.scala
@@ -0,0 +1,32 @@
+// Copyright (C) 2014-2018 Anduin Transactions Inc.
+
+package anduin.component.progressindicators
+
+// scalastyle:off underscore.import
+import japgolly.scalajs.react._
+import japgolly.scalajs.react.vdom.html_<^._
+// scalastyle:on underscore.import
+
+final case class GeneralLoader() {
+  def apply(): VdomElement = GeneralLoader.component(this)
+}
+
+object GeneralLoader {
+
+  private type Props = GeneralLoader
+
+  private val render: VdomElement = {
+    <.div(
+      ^.cls := "loader-general",
+      (0 to 4).toTagMod(_ => <.span(^.cls := "item"))
+    )
+  }
+
+  private val component = ScalaComponent
+    .builder[Props](this.getClass.getSimpleName)
+    .stateless
+    .renderStatic(render)
+    .build
+}
+
+
