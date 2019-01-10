1d95329aeacf65403c990d79b9023a4dd2a4bc07
Thien Do <dvkndn@gmail.com>
2019-01-10 10:01:22 +0700

    fix(platform): Drop RippleIndicator

--- src/main/scala/anduin/style/Animation.scala
+++ src/main/scala/anduin/style/Animation.scala
@@ -4,6 +4,5 @@ package anduin.style
 
 private[style] final case class Animation(classes: List[String] = List.empty) {
   def translateX330: Style = new Style(classes :+ "an-tx330")
-  def ripple: Style = new Style(classes :+ "an-ripple")
   def circle: Style = new Style(classes :+ "an-circle")
 }
