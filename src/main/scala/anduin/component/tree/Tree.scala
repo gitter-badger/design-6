7e0b1ed177218838103a85672738c25866c9359a
Thien Do <dvkndn@gmail.com>
2019-01-29 16:32:31 +0700

    fix(platform): Improve Tree component
    - Simplify Tree.Loader
    - Better "shouldExpanded" prop
    - Allow to render None instead of "shouldHidden"

--- src/main/scala/anduin/component/tree/Tree.scala
+++ src/main/scala/anduin/component/tree/Tree.scala
@@ -7,6 +7,7 @@ import anduin.component.icon.Icon
 import anduin.style.Style
 
 import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
 
 // scalastyle:off underscore.import
 import japgolly.scalajs.react._
@@ -14,124 +15,158 @@ import japgolly.scalajs.react.vdom.html_<^._
 // scalastyle:on underscore.import
 
 object Tree {
+
+  sealed trait Loader[A]
   object Loader {
-    case class Sync[A](loadSync: A => Seq[A]) extends TreeLoader.Sync[A]
-    case class Async[A](hasChildren: A => Boolean, loadAsync: A => Future[Seq[A]]) extends TreeLoader.Async[A]
+    case class Sync[A](loadSync: A => Seq[A]) extends Loader[A]
+    case class Async[A](hasChildren: A => Boolean, loadAsync: A => Future[Seq[A]]) extends Loader[A]
   }
+
+  // Props to use to render a node (see class Tree#render)
+  case class NodeRenderProps[A](
+    node: A,
+    // ancestors of current node, from furthest to closest (direct parent)
+    // - ie. ancestorNodes.tail is parent of current node
+    ancestorNodes: Seq[A],
+    toggle: Callback,
+    isExpanded: Boolean
+  )
 }
 
+// A is the type of a node
 class Tree[A] {
 
   def apply(): Props.type = Props
 
-  case class RenderProps(
-    node: A,
-    parents: Seq[A],
-    toggle: Callback,
-    isOpened: Boolean
-  )
-
   case class Props(
-    node: A,
-    render: RenderProps => VdomElement,
-    getKey: A => String,
-    loader: TreeLoader[A],
-    parentNodes: Seq[A] = Seq.empty,
-    shouldOpen: A => Boolean = _ => false,
-    isSelfHidden: Boolean = false
+    node: A, // The current node
+    getKey: A => String, // How to stringify a node
+    loader: Tree.Loader[A], // How to load child nodes of a node
+
+    // How to render a node.
+    // - Return Some to specify the right side of the ">" button
+    // - Return None to not render anything at all, including the ">" button
+    render: Tree.NodeRenderProps[A] => Option[VdomElement],
+    // The parent nodes of the current node.
+    // - This is mostly used to render a node (see Tree.NodeRenderProps) and
+    //   is passed by the Tree component to non-root nodes.
+    // - The consumers, who pass the root node, usually don't need this
+    ancestorNodes: Seq[A] = Seq.empty,
+    // Should a node expanded automatically
+    // - This will be called on both didMount and didUpdate
+    // - This is NOT a controlled prop. Think of this as getDerivedStateFromProps
+    //   which works like a condition when there are changes from parent to
+    //   turn state.isExpanded to true if it is currently false.
+    // - eg. if state.isExpanded is true already then this has no effect
+    // - eg. users can still turn state.isExpanded to false afterward
+    // - IMPORTANT: state.isExpanded is still the single source of truth
+    shouldExpanded: A => Boolean = _ => false
   ) {
     def apply(): VdomElement = component(this)
   }
 
-  case class State(isOpened: Boolean, children: Seq[A])
+  case class State(
+    isExpanded: Boolean,
+    // the "children" state should be used to store children in the case of
+    // asyncLoader only. For sync loader, simply get children on "render"
+    asyncChildren: Seq[A]
+  )
 
   private class Backend(scope: BackendScope[Props, State]) {
 
-    def setChildren(children: Seq[A]): Callback =
-      scope.modState(_.copy(children = children))
-
-    def setOpened(isOpened: Boolean): Callback =
-      scope.modState(_.copy(isOpened = isOpened))
-
-    private def toggle(props: Props, state: State): Callback = scope.modState(
-      _.copy(isOpened = !state.isOpened),
-      Callback.when(!state.isOpened)(props.loader.load(props.node, setChildren))
-    )
+    def setAsyncChildren(children: Seq[A]): Callback = scope.modState(_.copy(asyncChildren = children))
+    def setIsExpanded(isExpanded: Boolean): Callback = scope.modState(_.copy(isExpanded = isExpanded))
+    def toggle: Callback = scope.modState(state => state.copy(isExpanded = !state.isExpanded))
 
     private def renderButton(props: Props, state: State): VdomNode = {
-      if (props.loader.hasChildren(props.node)) {
-        val icon = if (state.isOpened) Icon.Glyph.CaretDown else Icon.Glyph.CaretRight
-        Button(
-          style = Button.Style.Minimal(icon = Some(icon), height = Button.Height.Fix24),
-          onClick = toggle(props, state)
-        )()
+      val hasChildren = props.loader match {
+        case loader: Tree.Loader.Async[A] => loader.hasChildren(props.node)
+        case loader: Tree.Loader.Sync[A]  => loader.loadSync(props.node).nonEmpty
+      }
+      if (hasChildren) {
+        val icon = if (state.isExpanded) Icon.Glyph.CaretDown else Icon.Glyph.CaretRight
+        val style = Button.Style.Minimal(icon = Some(icon), height = Button.Height.Fix24)
+        Button(style = style, onClick = toggle)()
       } else {
         <.div(^.width := "24px", ^.height := "24px")
       }
     }
 
-    private def renderChildren(props: Props, state: State): VdomNode = {
-      val children = state.children.toVdomArray { node =>
-        val element = props.copy(
-          node = node,
-          parentNodes = props.parentNodes :+ props.node,
-          shouldOpen = props.shouldOpen,
-          isSelfHidden = false
-        )()
+    private val indent = TagMod(
+        Style.border.left.borderColor.gray3.padding.left8,
+        ^.marginLeft := "11px" // 12px - 1px of the border
+      )
+
+    private def renderChildren(props: Props, state: State, content: Option[VdomElement]): VdomNode = {
+      val childrenNodes = props.loader match {
+        case _: Tree.Loader.Async[A]     => state.asyncChildren
+        case loader: Tree.Loader.Sync[A] => loader.loadSync(props.node)
+      }
+      val childrenVdoms = childrenNodes.toVdomArray { node =>
+        val ancestorNodes = props.ancestorNodes :+ props.node
+        val element = props.copy(node = node, ancestorNodes = ancestorNodes)()
         <.div(^.key := props.getKey(node), element)
       }
       <.div(
-        TagMod.when(!state.isOpened)(Style.display.none),
-        Style.border.left.borderColor.gray3.padding.left8,
-        ^.marginLeft := "11px", // 12px - 1px of the border
-        children
+        TagMod.when(!state.isExpanded)(Style.display.none),
+        TagMod.when(content.isDefined)(indent),
+        childrenVdoms
       )
     }
 
-    private def renderContent(props: Props, state: State): VdomElement = {
-      val rProps = RenderProps(
-        props.node,
-        props.parentNodes,
-        toggle(props, state),
-        state.isOpened
-      )
-      props.render(rProps)
+    private def renderBody(props: Props, state: State, contentOpt: Option[VdomElement]): Option[VdomElement] = {
+      contentOpt.map { content =>
+        <.div(
+          Style.flexbox.flex.flexbox.itemsStart,
+          <.div(Style.flexbox.none, renderButton(props, state)),
+          <.div(Style.flexbox.fixed, content)
+        )
+      }
     }
 
     def render(props: Props, state: State): VdomElement = {
+      val rProps = Tree.NodeRenderProps[A](props.node, props.ancestorNodes, toggle, state.isExpanded)
+      val content = props.render(rProps)
       <.div(
-        TagMod.unless(props.isSelfHidden)(
-          <.div(
-            Style.flexbox.flex.flexbox.itemsStart,
-            <.div(Style.flexbox.none, renderButton(props, state)),
-            <.div(Style.flexbox.fixed, renderContent(props, state))
-          )
-        ),
-        <.div(renderChildren(props, state))
+        <.div(renderBody(props, state, content)),
+        <.div(renderChildren(props, state, content))
       )
+    }
+  }
+
+  private def loadAsyncChildren(props: Props, state: State, cb: Seq[A] => Callback): Callback = {
+    Callback.when(state.isExpanded) {
+      props.loader match {
+        case async: Tree.Loader.Async[A] => Callback.future(async.loadAsync(props.node).map(cb))
+        case _: Tree.Loader.Sync[A]      => Callback.empty
+      }
     }
   }
 
   private val component = ScalaComponent
     .builder[Props](this.getClass.getSimpleName)
     .initialStateFromProps(props => {
-      State(isOpened = props.shouldOpen(props.node), children = Seq.empty)
+      State(isExpanded = props.shouldExpanded(props.node), asyncChildren = Seq.empty)
     })
     .renderBackend[Backend]
     .componentDidMount(scope => {
-      Callback.when(scope.state.isOpened) {
-        scope.props.loader.load(scope.props.node, scope.backend.setChildren)
-      }
+      loadAsyncChildren(scope.props, scope.state, scope.backend.setAsyncChildren)
     })
     .componentDidUpdate { scope =>
-      Callback.when(scope.prevProps.shouldOpen != scope.currentProps.shouldOpen) {
-        scope.backend.setOpened(scope.currentProps.shouldOpen(scope.currentProps.node))
-      } >>
-        Callback.when(
-          scope.prevProps.node != scope.currentProps.node || (scope.currentState.isOpened != scope.prevState.isOpened && scope.currentState.isOpened)
+      for {
+        // reload async children if needed
+        _ <- Callback.when(
+          scope.prevProps.node != scope.currentProps.node ||
+            scope.prevState.isExpanded != scope.currentState.isExpanded
         ) {
-          scope.currentProps.loader.load(scope.currentProps.node, scope.backend.setChildren)
+          loadAsyncChildren(scope.currentProps, scope.currentState, scope.backend.setAsyncChildren)
         }
+        // set isExpanded if needed
+        _ <- Callback.when({
+          val props = scope.currentProps
+          scope.prevProps != props && props.shouldExpanded(props.node)
+        })(scope.backend.setIsExpanded(true))
+      } yield ()
     }
     .build
 }
