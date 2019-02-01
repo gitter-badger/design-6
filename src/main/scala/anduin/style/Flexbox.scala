d1a89234e7f91c5c55d0dbc95d0bf9f519f960d1
Thien Do <dvkndn@gmail.com>
2019-02-01 13:41:11 +0700

    fix(platform): Style: Drop flex custom basis
    It's better to use flexBox.none + Style.width

--- src/main/scala/anduin/style/Flexbox.scala
+++ src/main/scala/anduin/style/Flexbox.scala
@@ -31,12 +31,5 @@ private[style] final case class Flexbox(classes: List[String] = List.empty) {
   // Children
   def fixed: Style = new Style(classes :+ "flex-fixed")
   def none: Style = new Style(classes :+ "flex-none")
-
-  def basis15: Style = new Style(classes :+ "basis-15")
-  def basis25: Style = new Style(classes :+ "basis-25")
-  def basis40: Style = new Style(classes :+ "basis-40")
-  def basis50: Style = new Style(classes :+ "basis-50")
-  def basis60: Style = new Style(classes :+ "basis-60")
-  def basis70: Style = new Style(classes :+ "basis-70")
 }
 // scalastyle:on number.of.methods
