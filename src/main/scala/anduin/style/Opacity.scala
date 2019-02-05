7d5ec21185c2079e7b0781a720337c8b6249ac23
Thien Do <dvkndn@gmail.com>
2019-02-05 21:51:45 +0700

    fix(platform): Opacity: Toward Tailwind
    `o-20` ~> `opacity-20`

--- src/main/scala/anduin/style/Opacity.scala
+++ src/main/scala/anduin/style/Opacity.scala
@@ -3,12 +3,15 @@
 package anduin.style
 
 private[style] final case class Opacity(classes: List[String] = List.empty) {
-  def pc0: Style = new Style(classes :+ "o-0")
-  def pc10: Style = new Style(classes :+ "o-10")
-  def pc20: Style = new Style(classes :+ "o-20")
-  def pc30: Style = new Style(classes :+ "o-30")
-  def pc50: Style = new Style(classes :+ "o-50")
-  def pc80: Style = new Style(classes :+ "o-80")
-  def pc90: Style = new Style(classes :+ "o-90")
-  def pc100: Style = new Style(classes :+ "o-100")
+  def pc0: Style = new Style(classes :+ "opacity-0")
+  def pc10: Style = new Style(classes :+ "opacity-10")
+  def pc20: Style = new Style(classes :+ "opacity-20")
+  def pc30: Style = new Style(classes :+ "opacity-30")
+  def pc40: Style = new Style(classes :+ "opacity-40")
+  def pc50: Style = new Style(classes :+ "opacity-50")
+  def pc60: Style = new Style(classes :+ "opacity-60")
+  def pc70: Style = new Style(classes :+ "opacity-70")
+  def pc80: Style = new Style(classes :+ "opacity-80")
+  def pc90: Style = new Style(classes :+ "opacity-90")
+  def pc100: Style = new Style(classes :+ "opacity-100")
 }
