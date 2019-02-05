acef213735dd7bc34918152d72ea5e74cf1a764a
dvkndn <dvkndn@gmail.com>
2019-02-06 03:01:40 +0700

    checkbox: complete

--- core/src/main/scala/anduin/guide/pages/components/checkbox/PageCheckbox.scala
+++ core/src/main/scala/anduin/guide/pages/components/checkbox/PageCheckbox.scala
@@ -25,7 +25,7 @@ object PageCheckbox {
       Toc(headings = Source.getTocHeadings)(),
       Markdown(
         """
-          |A checkbox allows users to mark an option as selected (checked) or
+          |Checkboxes allow users to mark options as selected (checked) or
           |not (unchecked):
         """.stripMargin
       )(),
@@ -37,6 +37,12 @@ object PageCheckbox {
           )("Remember me")
         })()
       }))(),
+      Markdown(
+        """
+          |Checkboxes are often used as a list, or even nested lists, to
+          |allows users to select no, one or several options:
+        """.stripMargin
+      )(),
       ChecklistExample(
         items = List(("Tomato", true), ("Orange", false), ("Apple", false)),
         parent = Some("Fruit")
@@ -49,6 +55,15 @@ object PageCheckbox {
           |isChecked: Boolean = false
           |onChange: Boolean => Callback = _ => Callback.empty
           |```
+          |
+          |Checkbox is a [stateless] component. Its consumers should define
+          |whether it is checked or not via the `isChecked` prop, as well as
+          |response to users' interactions via the `onChange` prop.
+          |
+          |In practice, `isChecked` is usually based on a boolean
+          |state, which is updated accordingly via `onChange`:
+          |
+          |[stateless]: https://reactjs.org/docs/forms.html#controlled-components
           |
           |```scala
           |// case class State(isFoo: Boolean, ...)
@@ -69,6 +84,9 @@ object PageCheckbox {
           |```scala
           |isDisabled: Boolean = false
           |```
+          |Set `isDisabled = true` to disable users' interaction with a
+          |checkbox. Disabled checkboxes can still reflect whether they are
+          |checked or not:
         """.stripMargin
       )(),
       ExampleRich(Source.annotate({
@@ -80,12 +98,22 @@ object PageCheckbox {
         )
       }))(),
       Markdown(
+        s"""
+          |However, avoid using disabled checkboxes for pure informational
+          |purpose. To show information without interaction, use [icons].
+          |
+          |[icons]: ${ctl.urlFor(Pages.IconGlyph("#actions")).value}
+        """.stripMargin
+      )(),
+      Markdown(
         """
           |# Indeterminate
           |
           |```scala
           |isIndeterminate: Boolean = false
           |```
+          |
+          |A checkbox can be displayed as indeterminate:
         """.stripMargin
       )(),
       ExampleRich(Source.annotate({
@@ -97,6 +125,18 @@ object PageCheckbox {
           )(s"Actual value: $value")
         })()
       }))(),
+      Markdown(
+        """
+          |As shown in the above example, "indeterminate" is **not an
+          |additional state** to checked and unchecked. It is only a visual
+          |feature that effectively hides the actual `isChecked` value.
+          |
+          |Because of this, the value of these checkboxes are usually not
+          |collected (for submission). They only work as parents of
+          |other checkboxes, allow users to check or uncheck all of them at
+          |once:
+        """.stripMargin
+      )(),
       ChecklistExample(
         items = List(
           ("Term Sheet", true),
@@ -107,7 +147,34 @@ object PageCheckbox {
       )(),
       Markdown(
         """
+          |Since checkbox is a stateless component, its consumers are
+          |responsible for controlling the `isIndeterminate` prop (as well as
+          |`isChecked` and `onChange`) properly if used.
+          |
+          |The below example should give you a general idea of controlling a
+          |checkbox that can be indeterminate:
+          |
+          |```scala
+          |// class Item(isSelected: Boolean)
+          |// class State(items: Seq[Item])
+          |isChecked = state.items.forall(_.isSelected)
+          |isIndeterminate = state.items.exists(item => {
+          |  state.items.exists(_.isSelected != item.isSelected)
+          |})
+          |onChange = isChecked => scope.modState { state =>
+          |  val items = state.items.map(_.copy(isSelected = isChecked))
+          |  state.copy(items = items)
+          |}
+          |```
+          |
+        """.stripMargin
+      )(),
+      Markdown(
+        """
           |# Text Wrap
+          |
+          |When there is not enough space, a checkbox's label is wrapped onto
+          |multiple lines, while the checkbox itself stays at top left:
         """.stripMargin
       )(),
       ExampleSimple()({
@@ -118,6 +185,13 @@ object PageCheckbox {
           )(sampleAgreement)
         })()
       }),
+      Markdown(
+        """
+          |However, it's generally better to keep your checkboxes' labels short
+          |and simple. Additional information can then be separated to a
+          |paragraph above or below it:
+        """.stripMargin
+      )(),
       ExampleSimple()({
         DemoState.Bool(initialValue = false, render = (value, onChange) => {
           <.div(
