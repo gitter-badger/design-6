7ffc12812dde1898853fd4b08aef9359bd138fbe
dvkndn <dvkndn@gmail.com>
2019-02-07 03:13:15 +0700

    fix(platform): WhiteSpace: Toward Tailwind

--- src/main/scala/anduin/style/WhiteSpace.scala
+++ src/main/scala/anduin/style/WhiteSpace.scala
@@ -3,9 +3,9 @@
 package anduin.style
 
 private[style] final case class WhiteSpace(classes: List[String] = List.empty) {
-  def normal: Style = new Style(classes :+ "ws-normal")
-  def noWrap: Style = new Style(classes :+ "nowrap")
-  def pre: Style = new Style(classes :+ "pre")
-  def preWrap: Style = new Style(classes :+ "pre-wrap")
-  def preLine: Style = new Style(classes :+ "pre-line")
+  def normal: Style = new Style(classes :+ "whitespace-normal")
+  def noWrap: Style = new Style(classes :+ "whitespace-no-wrap")
+  def pre: Style = new Style(classes :+ "whitespace-pre")
+  def preWrap: Style = new Style(classes :+ "whitespace-pre-wrap")
+  def preLine: Style = new Style(classes :+ "whitespace-pre-line")
 }
