fc2230ff33bd0a37677b4bdc2b5d05089b031bbc
dvkndn <dvkndn@gmail.com>
2019-02-06 22:43:37 +0700

    fix(platform): FontWeight: Toward Tailwind

--- src/main/scala/anduin/style/FontWeight.scala
+++ src/main/scala/anduin/style/FontWeight.scala
@@ -3,11 +3,8 @@
 package anduin.style
 
 private[style] final case class FontWeight(classes: List[String] = List.empty) {
-  def thin: Style = new Style(classes :+ "fw1")
-  def extraLight: Style = new Style(classes :+ "fw2")
-  def light: Style = new Style(classes :+ "fw3")
-  def normal: Style = new Style(classes :+ "fw4")
-  def medium: Style = new Style(classes :+ "fw5")
-  def semiBold: Style = new Style(classes :+ "fw6")
-  def bold: Style = new Style(classes :+ "fw7")
+  def normal: Style = new Style(classes :+ "font-normal")
+  def medium: Style = new Style(classes :+ "font-medium")
+  def semiBold: Style = new Style(classes :+ "font-semi-bold")
+  def bold: Style = new Style(classes :+ "font-bold")
 }
