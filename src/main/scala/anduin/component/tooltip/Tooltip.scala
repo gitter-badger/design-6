dd764a9cbebda76c6eefb09ae5a258e3b0c288a1
Thien Do <dvkndn@gmail.com>
2019-03-12 04:23:50 +0700

    feat(platform): Tooltip: Simplify implementation
    - Use PortalWrapper to simplify API
    - Better integration with Popper
    - Allow usage without target
    
    See https://anduin.design/tooltip

--- src/main/scala/anduin/component/tooltip/Tooltip.scala
+++ src/main/scala/anduin/component/tooltip/Tooltip.scala
@@ -2,8 +2,8 @@
 
 package anduin.component.tooltip
 
-import anduin.component.portal.{PortalPopper, Position, PositionTopCenter}
-import anduin.style.Style
+import anduin.component.portal.{PortalPosition, PortalWrapper}
+import org.scalajs.dom.raw.HTMLElement
 
 // scalastyle:off underscore.import
 import japgolly.scalajs.react._
@@ -11,66 +11,51 @@ import japgolly.scalajs.react.vdom.html_<^._
 // scalastyle:on underscore.import
 
 final case class Tooltip(
-  // Portal common props (see Portal for detail)
   renderTarget: VdomNode,
-  renderContent: () => VdomNode,
-  // PortalPopper common props (see PortalPopper for detail)
-  position: Position = PositionTopCenter,
-  targetTag: HtmlTag = <.div,
-  // Tooltip specific props
-  isDisabled: Boolean = false
+  renderContent: () => String,
+  position: PortalPosition = PortalPosition.TopCenter,
+  targetWrapper: PortalWrapper = PortalWrapper.BlockContent,
+  isDisabled: Boolean = true
 ) {
-  def apply(): VdomElement = { Tooltip.component(this) }
+  def apply(): VdomElement = Tooltip.component(this)
 }
 
 object Tooltip {
 
   private type Props = Tooltip
 
-  private def renderTarget(props: Props)(popper: PortalPopper.TargetProps) = {
-    props.targetTag.withRef(popper.ref)(
-      ^.onMouseEnter --> Callback.when(!popper.isOpened)(popper.toggle), // open
-      ^.onMouseLeave --> Callback.when(popper.isOpened)(popper.toggle), // close
-      props.renderTarget
+  private case class State(isOpened: Boolean)
+
+  private class Backend(scope: BackendScope[Props, State]) {
+
+    private val targetRef: Ref.Simple[HTMLElement] = Ref[HTMLElement]
+
+    private val eventListeners = TagMod(
+      ^.onMouseEnter --> scope.modState(_.copy(isOpened = true)),
+      ^.onMouseLeave --> scope.modState(_.copy(isOpened = false))
     )
-  }
 
-  private val contentStyles = TagMod(
-    Style.background.gray9.color.white.shadow.px8,
-    Style.padding.ver4.padding.hor8.borderRadius.px4,
-    // Backward compatible
-    // https://github.com/anduintransaction/stargazer/issues/17011
-    Style.zIndex.idx999
-  )
+    private def renderContent(props: Props, state: State): VdomNode = {
+      if (state.isOpened) {
+        TooltipContent(targetRef, props.position, props.renderContent())()
+      } else {
+        EmptyVdom
+      }
+    }
 
-  private def renderContent(props: Props)(popper: PortalPopper.ContentProps): VdomElement = {
-    val body = props.renderContent()
-    val arrow = <.div(popper.arrowMod, Style.background.gray9)
-    <.div.withRef(popper.ref)(popper.styles, contentStyles, body, arrow)
-  }
+    private def renderTarget(props: Props, state: State): VdomElement = {
+      val children = TagMod(props.renderTarget, eventListeners)
+      props.targetWrapper.tag.withRef(targetRef)(children)
+    }
 
-  private def render(props: Tooltip): VdomNode = {
-    if (props.isDisabled) {
-      // it is intentional to render targetTag wrapper here to keep the
-      // HTML result consistent with the other case
-      props.targetTag(props.renderTarget)
-    } else {
-      val arrowOffset = PortalPopper.getArrowOffset(props.position)
-      PortalPopper(
-        renderTarget = renderTarget(props),
-        renderContent = renderContent(props),
-        isOpened = None,
-        // ===
-        position = props.position,
-        offsetHor = arrowOffset._1,
-        offsetVer = arrowOffset._2
-      )()
+    def render(props: Props, state: State): VdomElement = {
+      React.Fragment(renderTarget(props, state), renderContent(props, state))
     }
   }
 
   private val component = ScalaComponent
     .builder[Props](this.getClass.getSimpleName)
-    .stateless
-    .render_P(render)
+    .initialState(State(isOpened = false))
+    .renderBackend[Backend]
     .build
 }
---
+++ src/main/scala/anduin/component/tooltip/TooltipContent.scala
@@ -0,0 +1,105 @@
+// Copyright (C) 2014-2019 Anduin Transactions Inc.
+
+package anduin.component.tooltip
+
+import anduin.component.portal.{PortalPopperContent, PortalPosition}
+import anduin.scalajs.popper.{Popper, PopperPlacement}
+import anduin.style.Style
+import org.scalajs.dom
+import org.scalajs.dom.raw.HTMLElement
+
+// scalastyle:off underscore.import
+import japgolly.scalajs.react._
+import japgolly.scalajs.react.vdom.html_<^._
+// scalastyle:on underscore.import
+
+final case class TooltipContent(
+  // To where the content should be anchored to
+  targetRef: Ref.Simple[HTMLElement],
+  // Same as Tooltip's `position` prop
+  position: PortalPosition = PortalPosition.TopCenter,
+  text: String
+) {
+  def apply(): VdomElement = TooltipContent.component(this)
+}
+
+object TooltipContent {
+
+  private type Props = TooltipContent
+
+  private val staticMod: TagMod = TagMod(
+    // Common popper content styles
+    PortalPopperContent.staticStyles,
+    // Specific styles of Tooltip
+    Style.background.gray9.color.white.shadow.px8,
+    Style.maxWidth.px256.padding.ver4.padding.hor8.borderRadius.px4,
+    // Allow line breaking
+    Style.whiteSpace.preWrap,
+    // Backward compatible
+    // https://github.com/anduintransaction/stargazer/issues/17011
+    Style.zIndex.idx999
+  )
+
+  // This will be set by Popper. The reason we need this is because:
+  // 1. Popper's actual placement is calculated outside of React world
+  // 2. We need the actual placement to properly align the arrow in both axises
+  //    (Popper only align the arrow in 1 axis)
+  // Having this in State allow React to update the arrow properly, and the
+  // code is much cleaner than we update the DOM directly
+  // - Note that this is NOT an equivalent of props.position, because Popper
+  //   might flip the position if there is not enough space
+  case class State(placement: PopperPlacement)
+
+  class Backend(scope: BackendScope[Props, State]) {
+
+    private val ref = Ref[HTMLElement]
+    private var popperOpt: Option[Popper] = None // scalastyle:ignore var.field
+
+    // Rendering
+
+    private def renderContent(props: Props, state: State): VdomElement = {
+      val arrow = TooltipArrow(placement = state.placement)()
+      <.div.withRef(ref)(staticMod, props.text, arrow)
+    }
+
+    def render(props: Props, state: State): VdomNode = {
+      println("render", state.placement)
+      ReactPortal(renderContent(props, state), dom.document.body)
+    }
+
+    // Lifecycle
+
+    private def setPlacement(data: Popper.Data): Unit = {
+      val placement = PopperPlacement
+        .fromString(data.placement)
+        .getOrElse(PopperPlacement.TopMiddle)
+      scope.modState(_.copy(placement = placement)).runNow()
+    }
+
+    def initPopper: Callback = {
+      for {
+        props <- scope.props
+        target <- props.targetRef.get
+        content <- ref.get
+        _ <- Callback {
+          val options = PortalPopperContent.getOptions(
+            onCreated = setPlacement,
+            position = props.position
+          )
+          this.popperOpt = Some(new Popper(target, content, options))
+        }
+      } yield ()
+    }
+
+    def destroyPopper: Callback =
+      Callback.traverseOption(popperOpt)(p => Callback(p.destroy()))
+  }
+
+  private val component = ScalaComponent
+    .builder[Props](this.getClass.getSimpleName)
+    .initialState(State(placement = PopperPlacement.TopMiddle))
+    .renderBackend[Backend]
+    .componentDidMount(_.backend.initPopper)
+    .componentWillUnmount(_.backend.destroyPopper)
+    .build
+}
