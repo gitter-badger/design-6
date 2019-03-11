6799880ff93f17997ec2b5d8b9a6a31441e9e77d
Thien Do <dvkndn@gmail.com>
2019-03-12 04:17:02 +0700

    feat(platform): Portal: Simplify PortalPosition
    - Rename Position object to PortalPosition
    - Move options to under PortalPosition object

---
+++ src/main/scala/anduin/component/portal/PortalPosition.scala
@@ -0,0 +1,28 @@
+// Copyright (C) 2014-2019 Anduin Transactions Inc.
+
+package anduin.component.portal
+
+import anduin.scalajs.popper.PopperPlacement
+
+// scalastyle:off underscore.import
+import anduin.scalajs.popper.PopperPlacement._
+// scalastyle:on underscore.import
+
+abstract class PortalPosition(
+  val popperPlacement: PopperPlacement
+)
+
+object PortalPosition {
+  object TopLeft extends PortalPosition(TopStart)
+  object TopCenter extends PortalPosition(TopMiddle)
+  object TopRight extends PortalPosition(TopEnd)
+  object RightTop extends PortalPosition(RightStart)
+  object RightCenter extends PortalPosition(RightMiddle)
+  object RightBottom extends PortalPosition(RightEnd)
+  object BottomLeft extends PortalPosition(BottomStart)
+  object BottomCenter extends PortalPosition(BottomMiddle)
+  object BottomRight extends PortalPosition(BottomEnd)
+  object LeftTop extends PortalPosition(LeftStart)
+  object LeftCenter extends PortalPosition(LeftMiddle)
+  object LeftBottom extends PortalPosition(LeftEnd)
+}
