61e990703c618bcadfc67aa4adbfbc0f3c8af3a1
dvkndn <dvkndn@gmail.com>
2019-03-14 16:10:47 +0700

    feat(platform): Icon: Product icons should export the recommended background color

--- src/main/scala/anduin/component/icon/IconProduct.scala
+++ src/main/scala/anduin/component/icon/IconProduct.scala
@@ -8,19 +8,23 @@ import japgolly.scalajs.react.vdom.svg_<^._
 // scalastyle:on underscore.import
 
 sealed trait IconProduct extends Icon.Name {
-  def paths: String
+  def bgColor: String
+  final def bgMod: TagMod = vdom.html_<^.^.backgroundColor := bgColor
 
+  protected def paths: String
   private def group = <.g(
     TagMod(^.fill := "none", ^.fillRule := "evenodd"),
     vdom.html_<^.^.dangerouslySetInnerHtml := paths
   )
-  final def sizeMods: Seq[Icon.SizeMod] = Vector(Icon.SizeMod(Icon.Size.Px32, group))
+  private[icon] final def sizeMods: Seq[Icon.SizeMod] =
+    Vector(Icon.SizeMod(Icon.Size.Px32, group))
 }
 
 // scalastyle:off line.size.limit
 object IconProduct {
   object Negotiation extends IconProduct {
-    val paths: String =
+    val bgColor: String = "#00A6B7"
+    protected val paths: String =
       """
         |<path fill="#FFF" d="M7 4h13l6 6v17a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V5a1 1 0 0 1 1-1z"/>
         |<path fill="#AEDEE6" d="M9 7h6v1.5H9V7zm0 4h14v1.5H9V11zm0 6h14v1.5H9V17zm0-3h14v1.5H9V14zm0 6h14v1.5H9V20zm0 3h10v1.5H9V23zM20 4v6h6z"/>
@@ -28,7 +32,8 @@ object IconProduct {
       """.stripMargin
   }
   object LegalDiligence extends IconProduct {
-    val paths: String =
+    val bgColor: String = "#15B8E6"
+    protected val paths: String =
       """
         |<path fill="#C5EDF8" d="M7.42 9h17.16a.5.5 0 0 1 .48.64l-.7 2.36H7.64l-.69-2.36A.5.5 0 0 1 7.42 9z"/>
         |<path fill="#78D6F0" d="M10.65 5h10.7a1 1 0 0 1 .96 1.28L21.8 8H10.2l-.51-1.72A1 1 0 0 1 10.65 5z"/>
@@ -36,7 +41,8 @@ object IconProduct {
       """.stripMargin
   }
   object Signature extends IconProduct {
-    val paths: String =
+    val bgColor: String = "#9190E5"
+    protected val paths: String =
       """
         |<path fill="#FFF" d="M24.98 9.1l-.01 7.12a4 4 0 0 1-1.53 3.14L18 23.65c-.7.55-1.73.44-2.29-.25a1.6 1.6 0 0 1-.18-.29l-3.09-6.13a4 4 0 0 1-.04-3.5l3.04-6.46c-.38-.28 10.02 1.99 9.55 2.08z"/>
         |<path fill="#7171D2" d="M15.03 5.76l.66-2.8a1 1 0 0 1 1.18-.76L26 4.12a1 1 0 0 1 .77 1.2l-.65 2.77c-.11.47-.5.81-.96.91L15.5 6.97a1.16 1.16 0 0 1-.47-1.21z"/>
@@ -46,14 +52,16 @@ object IconProduct {
       """.stripMargin
   }
   object Wiring extends IconProduct {
-    val paths: String =
+    val bgColor: String = "#30A594"
+    protected val paths: String =
       """
         |<path fill="#FFF" d="M7 23h19a1 1 0 0 1 1 1v1a1 1 0 0 1-1 1H5.5a.5.5 0 0 1-.5-.5v-2a.5.5 0 0 1 .5-.5H7zm19.5-11h-21a.5.5 0 0 1-.5-.5V9.35a.5.5 0 0 1 .33-.47L16 5l10.67 3.88a.5.5 0 0 1 .33.47v2.15a.5.5 0 0 1-.5.5z"/>
         |<path fill="#79E6D5" d="M16 10a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm-9 2h2v11H7zm4 1h2v9h-2zm4 0h2v9h-2zm4 0h2v9h-2zm4-1h2v11h-2z"/>
       """.stripMargin
   }
   object Closing extends IconProduct {
-    val paths: String =
+    val bgColor: String = "#0AAB68"
+    protected val paths: String =
       """
         |<path fill="#FFF" d="M7 8.66l16.42-4.4a1 1 0 0 1 1.23.7l2.84 10.63-17.38 4.66L7 8.65z"/>
         |<path fill="#BEE6D5" d="M4 9v16a1 1 0 0 0 1 1h22a1 1 0 0 0 1-1V12a1 1 0 0 0-1-1h-9.91L14 8H5a1 1 0 0 0-1 1z"/>
