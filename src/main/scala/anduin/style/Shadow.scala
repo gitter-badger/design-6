62afae2b35cc9b583762bce01114b3e60df4fb0a
dvkndn <dvkndn@gmail.com>
2019-03-20 18:22:39 +0700

    feat(platform): Style: Add 4-pixel shadow
    This is necessary for Card

--- src/main/scala/anduin/style/Shadow.scala
+++ src/main/scala/anduin/style/Shadow.scala
@@ -5,6 +5,7 @@ package anduin.style
 private[style] final case class Shadow(classes: List[String] = List.empty) {
   def px1Light: Style = new Style(classes :+ "shadow-1-light")
   def px1Dark: Style = new Style(classes :+ "shadow-1-dark")
+  def px4: Style = new Style(classes :+ "shadow-4")
   def px8: Style = new Style(classes :+ "shadow-8")
   def px12: Style = new Style(classes :+ "shadow-12")
   def hoverPx1Light: Style = new Style(classes :+ "hover:shadow-1-dark")
