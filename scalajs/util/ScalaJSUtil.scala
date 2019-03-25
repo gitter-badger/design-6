184b6c57cab846acfafe2584bdddff1ebf454575
dvkndn <dvkndn@gmail.com>
2019-03-25 09:38:07 +0700

    feat(platform): Improve ScalaJSUtil
    - Rename `anduin.scalajs.util.Util` to `anduin.scalajs.util.ScalaJSUtil`
      to enhance IDE's code completion. These are utils for dealing with
      js land after all.
    - Remove the unused and confusing `safelyToOption`
    - Rename `getModsFromProps` to `jsPropsToTagMod` to clarify its job
    - Add `jsNullToOption` to safely work with **js-based** `null` (e.g.
      `null`s from library's API)

--- scalajs/util/ScalaJSUtil.scala
+++ scalajs/util/ScalaJSUtil.scala
@@ -4,27 +4,30 @@ package anduin.scalajs.util
 
 import scala.collection.mutable
 import scala.scalajs.js
+import scala.scalajs.js.|
 
 // scalastyle:off underscore.import
 import japgolly.scalajs.react.vdom.html_<^._
 // scalastyle:on underscore.import
 
-object Util {
+object ScalaJSUtils {
 
-  // This works like `undefOr.toOption` but convert `null` to `None` instead of
-  // Some(null). To be specific:
-  // A            => Some(A)
-  // null         => None
-  // js.undefined => None
-  def safelyToOption[A](a: js.UndefOr[A]): Option[A] =
-    a.toOption.filter(_ != null)
-
-  def getModsFromProps(props: mutable.Map[String, js.Any]): TagMod = {
+  def jsPropsToTagMod(props: mutable.Map[String, js.Any]): TagMod = {
     // Although "props" comes directly from JS land, ScalaJS sometimes add
     // some internal attributes (like `"bitmap$init$0$2":true`). We need to get
     // rid of them before converting this "props" to a TagMod
     // See also: https://github.com/scala-js/scala-js/issues/2227
     val filtered = props.filterNot(_._1.contains("bitmap$"))
     filtered.map(prop => VdomAttr(prop._1) := prop._2).toTagMod
+  }
+
+  // Dealing with null in scala-js
+  // - Note that this is different than wrapping the value inside Option's
+  //   apply, which returns `Option[A | Null]`. This instead returns
+  //   `Option[A]`, allows easier usage
+  // - ref: https://github.com/scala-js/scala-js/issues/2344
+  @SuppressWarnings(Array("org.wartremover.warts.AsInstanceOf")) // scalastyle:ignore
+  def jsNullToOption[A](value: A | Null): Option[A] = {
+    if (value == null) None else Option(value.asInstanceOf[A])
   }
 }
