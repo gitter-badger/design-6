507ff271b89c584aa4082f2341235111f8b756ad
dvkndn <dvkndn@gmail.com>
2019-03-25 09:55:53 +0700

    fix(platform): Downshift: Improve facades
    - Clarify that `item` might be null
    - Add `inputValue` and `onInputValueChange` to allow controlled usage
      (this will be seen in Suggest component)
    - Explain why we don't have type annotation for the public "component"

--- scalajs/downshift/Downshift.scala
+++ scalajs/downshift/Downshift.scala
@@ -13,22 +13,24 @@ import japgolly.scalajs.react._
 // A for Item
 class Downshift[A] {
 
-  // Also alias for public uses
   private type RenderProps = DownshiftRenderProps[A]
   private type State = DownshiftState[A]
   private type StateChanges = DownshiftStateChanges[A]
 
   class Props(
     val onChange: js.Function1[A, Unit],
-    val itemToString: js.Function1[A, String],
+    val itemToString: js.Function1[A | Null, String],
     val stateReducer: js.Function2[State, StateChanges, StateChanges],
     val children: js.Function1[RenderProps, raw.React.Node],
     // ===
+    val onInputValueChange: js.UndefOr[js.Function1[String, Unit]],
+    val inputValue: js.UndefOr[String],
     val selectedItem: js.UndefOr[A | Null]
   ) extends js.Object
 
-  val component =
-    JsComponent[Props, Children.None, Null](Downshift)
+  // Don't try to type this. It's a complicated internal type of ScalaJSReact
+  // that eventually will be VdomElement
+  val component = JsComponent[Props, Children.None, Null](Downshift)
 }
 
 @JSImport("downshift", JSImport.Default)
