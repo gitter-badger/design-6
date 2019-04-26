5f75d9cfa60858cc2eedadb684cd364b52dbd814
dvkndn <dvkndn@gmail.com>
2019-04-26 18:07:26 +0700

    fix(platform): ErrorMessage: Send email instead of opening page

--- src/main/scala/anduin/component/error/ErrorMessage.scala
+++ src/main/scala/anduin/component/error/ErrorMessage.scala
@@ -28,27 +28,30 @@ object ErrorMessage {
   private type Props = ErrorMessage
 
   private val break = "\r\n"
+  private var isShowing = false // scalastyle:ignore var.field
 
-  private val supportLink = "https://www.anduintransact.com/contact"
+  private def getDetail(throwable: Throwable): String = dom.window.btoa {
+    List(
+      dom.document.location.href,
+      throwable.toString,
+      throwable.getStackTrace.map(_.toString).mkString(break)
+    ).mkString(break)
+  }
 
-  private val renderBody: VdomElement = ModalBody()(
+  private def renderBody(props: Props): VdomElement = ModalBody()(
     <.p(
-      "Please save all work in progress and reload the page. ",
-      "If the problem persists, please ",
-      <.a(^.href := supportLink, ^.target.blank, "contact support"),
+      "Please save all work in progress and reload the page.",
+      " If the problem persists, please ",
+      <.a(
+        ^.href := "mailto:support@anduintransact.com" +
+          "?subject=Help%20and%20support" +
+          s"&body=Error%20detail%3A%20${getDetail(props.throwable)}%0A",
+        ^.target.blank,
+        "contact support"
+      ),
       " and include the error detail in your message."
     )
   )
-
-  private def getDetail(throwable: Throwable): String =
-    s"Please give the following to our support at $supportLink:$break" +
-      dom.window.btoa {
-        List(
-          dom.document.location.href,
-          throwable.toString,
-          throwable.getStackTrace.map(_.toString).mkString(break)
-        ).mkString(break)
-      }
 
   private def renderFooter(props: Props, close: Callback): VdomNode = ModalFooter()(
     <.div(
@@ -56,13 +59,18 @@ object ErrorMessage {
       <.div(Style.flexbox.fill)(
         ClipboardCopy(
           render = (copy, isJustCopied) => {
-            val label = if (isJustCopied) "Copied to clipboard" else "Copy error detail"
-            Button(onClick = copy)(label)
+            Button(onClick = copy)(
+              if (isJustCopied) "Copied to clipboard" else "Copy error detail"
+            )
           },
-          text = getDetail(props.throwable)
+          text = "Please give the following to our support at " +
+            s"https://www.anduintransact.com/contact:$break" +
+            s"${getDetail(props.throwable)}\n"
         )()
       ),
-      <.div(Style.flexbox.none.margin.right8)(Button(onClick = close)("Reload later")),
+      <.div(Style.flexbox.none.margin.right8)(
+        Button(onClick = close)("Reload later")
+      ),
       <.div(Style.flexbox.none)(
         Button(
           style = Button.Style.Full(color = Button.Color.Primary),
@@ -72,24 +80,31 @@ object ErrorMessage {
     )
   )
 
-  private def render(props: Props): VdomElement = {
-    Modal(
-      title = "We encountered an unexpected problem",
-      renderContent = close => React.Fragment(renderBody, renderFooter(props, close)),
-      isClosable = None,
-      defaultIsOpened = true,
-      beforeClose = Callback.alert(
-        """
-          |Please reload the page as soon as possible. You might lose any
-          |unsaved changes if you continue using without reloading the page.
-        """.stripMargin
-      )
-    )()
+  private def render(props: Props): VdomNode = {
+    if (!isShowing) {
+      Modal(
+        title = "We encountered an unexpected problem",
+        renderContent = close => {
+          React.Fragment(renderBody(props), renderFooter(props, close))
+        },
+        isClosable = None,
+        defaultIsOpened = true,
+        beforeClose = Callback(isShowing = false) >> Callback.alert(
+          """
+            |Please reload the page as soon as possible. You might lose any
+            |unsaved changes if you continue using without reloading the page.
+          """.stripMargin
+        )
+      )()
+    } else {
+      EmptyVdom
+    }
   }
 
   private val component = ScalaComponent
     .builder[Props](this.getClass.getSimpleName)
     .stateless
     .render_P(render)
+    .componentDidMount(_ => Callback(isShowing = true))
     .build
 }
