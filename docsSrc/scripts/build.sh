5633f7a634831e8a462f50ecb5da3df20ccfe297
Thien Do <dvkndn@gmail.com>
2019-05-05 06:38:01 +0700

    docs: prepare build script

--- docsSrc/scripts/build.sh
+++ docsSrc/scripts/build.sh
@@ -1,35 +1,20 @@
 #!/usr/bin/env bash
 
-. ./scripts/_prepare.sh
-
-GREEN='\033[0;32m'
-DIM='\033[0;2m'
 PURPLE='\033[0;35m'
-NC='\033[0m'
+RESET='\033[0m'
 
-# Clean
-echo -e "${PURPLE}1/5${NC} Clearing previous generation..."
-rm -rf ${docs}
-mkdir ${docs}
+echo -e "${PURPLE}docs-build 1/4${RESET} Clean docs..."
+# rm -rf ${docs}
+# mkdir ${docs}
 
-# Resources
-echo -e "${PURPLE}2/5${NC} Copying resources (public content)..."
-cp -R ${core_res}/public/. ${docs}/
+echo -e "${PURPLE}docs-build 2/4${RESET} Build docs/core-style.css..."
+yarn run core-style
 
-# CSS
-echo -e "${PURPLE}3/5${NC} Compiling CSS..."
-postcss ${core_css_main} --config ${core_css_config} --output ${docs}/app.css
+echo -e "${PURPLE}docs-build 3/4${RESET} Build docs/app.js..."
+sbt docsSrc/fullOptJS::webpack
+# cp ${core_target}/core-opt-bundle.js ${docs}/app.js
 
-# JS
-echo -e "${PURPLE}4/5${NC} Compiling JS (sbt)..."
-sbt fullOptJS::webpack
+echo -e "${PURPLE}docs-build 4/4${RESET} Copy public assets to docs..."
+# cp -R ${core_res}/public/. ${docs}/
 
-echo -e "${PURPLE}5/5${NC} Copying JS..."
-cp ${core_target}/core-opt-bundle.js ${docs}/app.js
-
-# Temporarily give up scalajs-split
-# echo -e "${PURPLE}5/5${NC} Optimizing JS (scalajs-split)..."
-# npx scalajs-split ./core
-# cp -r ${core_target}/scalajs-split/scripts ${docs}/scripts
-
-echo -e "${GREEN}success${NC} Built at ./docs!"
+echo -e "${PURPLE}docs-build success${RESET} Built at ./docs!"
