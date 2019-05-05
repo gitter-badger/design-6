abfa0ba78f18ccd91d0cd35c95a5c282c6666662
Thien Do <dvkndn@gmail.com>
2019-05-05 17:14:36 +0700

    docs: update build script

--- docsSrc/scripts/build.sh
+++ docsSrc/scripts/build.sh
@@ -3,18 +3,24 @@
 PURPLE='\033[0;35m'
 RESET='\033[0m'
 
-echo -e "${PURPLE}docs-build 1/4${RESET} Clean docs..."
-# rm -rf ${docs}
-# mkdir ${docs}
+ROOT=$(dirname $0)/../../
 
-echo -e "${PURPLE}docs-build 2/4${RESET} Build docs/core-style.css..."
+echo -e "${PURPLE}docs/build 1/4${RESET} Clearing docs..."
+rm -rf ${ROOT}/docs
+mkdir ${ROOT}/docs
+
+echo -e "${PURPLE}docs/build 2/4${RESET} Building docs/core-style.css..."
 yarn run core-style
+cp    ${ROOT}/core/target/style.css       ${ROOT}/docs/core-style.css
 
-echo -e "${PURPLE}docs-build 3/4${RESET} Build docs/app.js..."
+echo -e "${PURPLE}docs/build 3/4${RESET} Building docs/app.js..."
 sbt docsSrc/fullOptJS::webpack
-# cp ${core_target}/core-opt-bundle.js ${docs}/app.js
+JS_OUTPUT=${ROOT}/docsSrc/target/scala-2.12/scalajs-bundler/main
+cp    ${JS_OUTPUT}/docssrc-opt.js         ${ROOT}/docs/app.js
+cp    ${JS_OUTPUT}/docssrc-opt-library.js ${ROOT}/docs/app-library.js
+cp    ${JS_OUTPUT}/docssrc-opt-loader.js  ${ROOT}/docs/app-loader.js
 
-echo -e "${PURPLE}docs-build 4/4${RESET} Copy public assets to docs..."
-# cp -R ${core_res}/public/. ${docs}/
+echo -e "${PURPLE}docs/build 4/4${RESET} Copying public assets to docs..."
+cp -R ${ROOT}/docsSrc/public/.            ${ROOT}/docs
 
-echo -e "${PURPLE}docs-build success${RESET} Built at ./docs!"
+echo -e "${PURPLE}docs/build success${RESET} Built at ./docs!"
